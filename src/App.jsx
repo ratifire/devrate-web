6ff74748441fe243d40c12c04b9df308ba6be31a
Alisa Apotovska <masanaapot@gmail.com>
2024-01-13 11:36:32 +0200

    Set up a new React application using create-react-app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+function App() {
+    return (
+        <div className="App">
+            <main>
+                <p>
+                    DEVRATE
+                </p>
+            </main>
+        </div>
+    );
+}
+
+export default App;
