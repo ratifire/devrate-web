ff2f70abced1085fed0d9e99ae3da2039e9b5110
Volodymyr Labunets <91912993+vplabunets@users.noreply.github.com>
2024-02-07 14:56:57 +0200

    Reworked Layout
    Layout was reworked. Makrup was changed.
    Mui Container was added with main background.
    Also I added normalization of CSS to index.css

--- src/pages/Layout .jsx
+++ src/pages/Layout .jsx
@@ -1,47 +1,30 @@
 import React from 'react';
-import {Outlet, Link} from 'react-router-dom';
-
-import {ToastContainer, Zoom} from 'react-toastify';
+import { Outlet } from 'react-router-dom';
+import { ToastContainer, Zoom } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import Container from '@mui/material/Container';
+
 const Layout = () => {
-    return (
-        <div>
-            <header>
-                {/*there will be the Header component instead of nav here later*/}
-                <nav>
-                    <ul className={'navList'}>
-                        <li>
-                            <Link to={`/`}>Home page</Link>
-                        </li>
-                        <li>
-                            <Link to={`/signup`}>Sign up</Link>
-                        </li>
-                        <li>
-                            <Link to={`/profile`}>Profile</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-            <main>
-                <ToastContainer
-                    position='top-right'
-                    autoClose={5000}
-                    limit={3}
-                    hideProgressBar={false}
-                    newestOnTop={false}
-                    closeOnClick
-                    rtl={false}
-                    pauseOnFocusLoss
-                    draggable
-                    pauseOnHover
-                    theme='colored'
-                    transition={Zoom}
-                />
-                <Outlet/>
-            </main>
-        </div>
-    );
+  return (
+    <Container maxWidth='xl' minWidth='xs' sx={{ background: '#1D1D1D', height: '100vh' }}>
+      <ToastContainer
+        position='top-right'
+        autoClose={5000}
+        limit={3}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='colored'
+        transition={Zoom}
+      />
+      <Outlet />
+    </Container>
+  );
 };
 
 export default Layout;
