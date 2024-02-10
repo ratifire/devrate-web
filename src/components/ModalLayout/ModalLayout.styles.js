0c5932ee0e6bf1ae01ed804ea7710892e3e9e26a
Volodymyr Labunets <91912993+vplabunets@users.noreply.github.com>
2024-02-10 12:14:58 +0200

    add Modal Layout

---
+++ src/components/ModalLayout/ModalLayout.styles.js
@@ -0,0 +1,24 @@
+export const styles = {
+  modal: () => ({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }),
+  modalContainer: () => ({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '55px 78px',
+    backgroundColor: '#1D1D1D',
+    width: 526,
+    minHeight: 660,
+  }),
+  iconContainer: () => ({
+    width: 147,
+    height: 18,
+  }),
+  icon: () => ({
+    width: 147,
+    height: 18,
+  }),
+};
