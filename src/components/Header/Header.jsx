3b46b4943e578590b5ebf05c0461c9d9e3ec612b
Alisa Apotovska <masanaapot@gmail.com>
2024-02-14 17:01:24 +0200

    added header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import { AppBar, Box, Toolbar, IconButton, Drawer, Divider, Link, Container } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import logo from '../../assets/icons/logo.svg';
+import styles from './Header.styles';
+import { useTranslation } from 'react-i18next';
+import { Link as RouterLink } from 'react-router-dom';
+
+function Header() {
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const { t } = useTranslation();
+
+  const handlerDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+  const navLinks = [t('header.about_platform'), t('header.community'), t('header.contacts'), t('header.log_in')];
+
+  const drawer = (
+    <Box onClick={handlerDrawerToggle} sx={styles.drawer}>
+      <Box sx={styles.logoMobileBox}>
+        <img src={logo} alt='logo' width={'187'} height={'22.7'} />
+      </Box>
+      <Divider />
+      <Box sx={styles.headerNavMobile}>
+        {navLinks.map((link) => (
+          <Link key={link} to={`/`} component={RouterLink} sx={styles.link}>
+            {link}
+          </Link>
+        ))}
+      </Box>
+    </Box>
+  );
+  return (
+    <AppBar component='header' position={'static'} sx={styles.header}>
+      <Container maxWidth='xl' sx={{ '@media (min-width: 600px)': { paddingX: '12px' } }}>
+        <Box sx={styles.wrapper}>
+          <Toolbar disableGutters sx={styles.toolbar}>
+            <IconButton
+              color='primary'
+              aria-label='open drawer'
+              edge='start'
+              sx={{
+                mr: 15,
+                display: { md: 'none' },
+              }}
+              onClick={handlerDrawerToggle}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Box sx={styles.logoBox}>
+              <img src={logo} alt='logo' width={'187'} height={'22.7'} />
+            </Box>
+            <Box sx={styles.headerNav}>
+              {navLinks.map((link) => (
+                <Link key={link} to={`/`} component={RouterLink} sx={styles.link}>
+                  {link}
+                </Link>
+              ))}
+            </Box>
+          </Toolbar>
+          <Box component='nav'>
+            <Drawer
+              variant='temporary'
+              open={mobileOpen}
+              onClose={handlerDrawerToggle}
+              sx={{
+                display: { sm: 'block', md: 'none' },
+                '& .MuiDrawer-paper': {
+                  boxSizing: 'border-box',
+                  width: '80wh',
+                },
+              }}
+            >
+              {drawer}
+            </Drawer>
+          </Box>
+        </Box>
+      </Container>
+    </AppBar>
+  );
+}
+
+export default Header;
---
+++ src/components/Header/Header.styles.js
@@ -0,0 +1,45 @@
+const styles = {
+  container: () => ({
+    maxWidth: 1248,
+  }),
+  header: () => ({
+    background: 'none',
+    borderBottom: '0.3px solid rgba(161, 161, 170, 0.3)',
+    boxShadow: 0,
+  }),
+  toolbar: () => ({
+    justifyContent: 'space between',
+    paddingY: 28.5,
+  }),
+  logoBox: () => ({
+    flexGrow: 1,
+    lineHeight: 0,
+  }),
+  logoMobileBox: () => ({
+    lineHeight: 0,
+    marginY: 15,
+  }),
+  headerNav: () => ({
+    display: { xs: 'none', md: 'flex' },
+    gap: 30,
+  }),
+  headerNavMobile: () => ({
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 30,
+    marginTop: 15,
+  }),
+  link: (theme) => ({
+    marginLeft: 0,
+    fontWeight: 500,
+    fontSize: 16,
+    lineHeight: '24px',
+    color: theme.palette.text.secondary,
+    textDecoration: 'none',
+  }),
+  drawer: () => ({
+    paddingTop: 20,
+    textAlign: 'center',
+  }),
+};
+export default styles;
