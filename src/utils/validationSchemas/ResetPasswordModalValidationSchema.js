75de0eef6bf11eeda4162b2a283473fdefebb5df
VladyslavDykyi <vladislavdykyi33@gmail.com>
2024-02-28 01:25:20 +0200

    fix validationSchemas #93

--- src/utils/validationSchemas/ResetPasswordModalValidationSchema.js
+++ src/utils/validationSchemas/ResetPasswordModalValidationSchema.js
@@ -1,16 +1,14 @@
 import * as Yup from 'yup';
-import { useTranslation } from 'react-i18next';
+import i18n from 'i18next';
 
 export const ResetPasswordModalValidationSchema = () => {
-  const { t } = useTranslation();
-
   return Yup.object().shape({
     password: Yup.string()
-      .min(6, t('inputs_errors.password_short'))
-      .max(50, t('inputs_errors.password_long'))
-      .required(t('inputs_errors.required')),
+      .min(6, i18n.t('inputs_errors.password_short'))
+      .max(50, i18n.t('inputs_errors.password_long'))
+      .required(i18n.t('inputs_errors.required')),
     repeat_password: Yup.string()
-      .oneOf([Yup.ref('password'), null], t('inputs_errors.must_match'))
-      .required(t('inputs_errors.required')),
+      .oneOf([Yup.ref('password'), null], i18n.t('inputs_errors.must_match'))
+      .required(i18n.t('inputs_errors.required')),
   });
 };
