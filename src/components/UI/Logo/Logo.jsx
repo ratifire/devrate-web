f1f28b93791005037680fe3cb4d1e7fc7847d3d9
Volodymyr Labunets <91912993+vplabunets@users.noreply.github.com>
2024-03-03 11:43:56 +0200

    update Logo component

--- src/components/UI/Logo/Logo.jsx
+++ src/components/UI/Logo/Logo.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import logo from '../../../assets/icons/logo.svg';
 import PropTypes from 'prop-types';
+import { CardMedia } from '@mui/material';
+import { pictureData } from '../../../utils/constants/pictureData';
 
-// TODO: use picture tag, debug in mozilla dev edition
 const Logo = ({ width, height }) => {
-  return <img src={logo} alt='logo' width={width} height={height} />;
+  return (
+    <CardMedia component='picture' width={width} height={height}>
+      {pictureData.map((element) => (
+        <source
+          key={element.id}
+          srcSet={`${element.src1x} 1x, ${element.src1x} 2x`}
+          media={`(max-width: ${element.mediaWidth}px)`}
+        />
+      ))}
+      <img src={logo} alt='logo' width={width} height={height} />
+    </CardMedia>
+  );
 };
 Logo.propTypes = {
   width: PropTypes.string,
