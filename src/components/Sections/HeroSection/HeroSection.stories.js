903925dd421b75724e5201581533d9375094fcf7
Dmitrij Duvanov <dmitrijduvanov7@gmail.com>
2024-03-11 21:07:50 +0200

    draft stories file added

---
+++ src/components/Sections/HeroSection/HeroSection.stories.js
@@ -0,0 +1,16 @@
+// import React from 'react';
+import HeroSection from './HeroSection';
+
+export default {
+  title: 'Example/HeroSection',
+  component: HeroSection,
+  tags: ['autodocs'],
+};
+
+export const Default = {};
+
+// const Template = () => <HeroSection/> 
+
+// export const Default = Template.bind({});
+
+// Default.args = {};
