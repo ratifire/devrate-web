6f3a017df1a375afe9079e373133f24cc8084a7f
VladyslavDykyi <vladislavdykyi33@gmail.com>
2024-03-11 14:39:47 +0200

    create a story file for the AboutSection component #133

---
+++ src/components/Sections/AboutSection/AboutSection.stories.js
@@ -0,0 +1,15 @@
+import AboutSection from './AboutSection';
+
+export default {
+  title: 'Example/AboutSection',
+  component: AboutSection,
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'dark',
+    },
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+};
+export const Default = {};
--- src/components/Sections/index.js
+++ src/components/Sections/index.js
@@ -1,6 +1,6 @@
 import Header from './Header/Header';
 import HeroSection from './HeroSection/HeroSection';
-import AboutSection from './AboutSection/AboutSection';
+import AboutSection from './AboutSection';
 import Footer from './Footer/Footer';
 
 export { Header, HeroSection, AboutSection, Footer };
