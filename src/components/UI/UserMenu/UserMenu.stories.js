e3540fa7d51a03b8c3aaeeeb4477a835f987b95d
BlackPuma9 <yuliiayavorska9@gmail.com>
2024-05-05 15:49:11 +0200

    Updated UserMenu.stories.js file and added states of the component

--- src/components/UI/UserMenu/UserMenu.stories.js
+++ src/components/UI/UserMenu/UserMenu.stories.js
@@ -1,5 +1,7 @@
-import { decorators } from '../../../../.storybook/preview';
+import React from 'react';
 import UserMenu from './UserMenu';
+import { decorators } from '../../../../.storybook/preview';
+
 
 export default {
   title: 'UI/UserMenu',
@@ -11,5 +13,9 @@ export default {
   decorators: decorators,
 };
 
+export const Default = () => <UserMenu isDrawerOpen toggleDrawer={decorators.action('toggleDrawer')} />;
 
+export const Open = () => <UserMenu isDrawerOpen={true} toggleDrawer={decorators.action('toggleDrawer')} />;
+
+export const Closed = () => <UserMenu isDrawerOpen={false} toggleDrawer={decorators.action('toggleDrawer')} />;
 
