e0cc66c5b5df2aef58330298ba1c1b4e0c8f643f
Volodymyr Labunets <91912993+vplabunets@users.noreply.github.com>
2024-05-06 20:26:01 +0300

    Refactor styles in Homepage/HeroSection

--- src/components/Sections/HeroSection/HeroSection.styles.js
+++ src/components/Sections/HeroSection/HeroSection.styles.js
@@ -1,18 +1,18 @@
 export const styles = {
-  container: {
-    '@media (min-width: 600px)': { paddingX: '12px' },
-  },
+  container: (theme) => ({
+    '@media (min-width: 600px)': { paddingX: theme.spacing(2) },
+  }),
   contentWrapper: {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    paddingTop: 149,
-    paddingBottom: 204,
+    paddingTop: '148px',
+    paddingBottom: '204px',
   },
   title: (theme) => ({
     maxWidth: 1100,
     textAlign: 'center',
-    marginBottom: 30,
+    marginBottom: theme.spacing(4),
     fontSize: 60,
     fontWeight: 700,
     lineHeight: '80px',
@@ -23,16 +23,8 @@ export const styles = {
     '&-webkit-text-fill-color': 'linear-gradient(90deg, #7562e4 0%, #fb93a6 100%)',
   }),
   text: (theme) => ({
-    marginBottom: 60,
+    marginBottom: '60px',
     textAlign: 'center',
-    fontWeight: 500,
-    fontSize: 22,
-    rowHeight: '150%',
     color: theme.palette.neutral[200],
   }),
-  button: {
-    paddingX: 0,
-    paddingY: 16,
-    width: 190,
-  },
 };
