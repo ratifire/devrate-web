d398a3303ff4f126871d60831b22cc68c132cf40
VladyslavDykyi <vladislavdykyi33@gmail.com>
2024-05-10 00:00:56 +0300

    UserAvatar.jsx_#247

--- src/components/UI/UserAvatar/UserAvatar.jsx
+++ src/components/UI/UserAvatar/UserAvatar.jsx
@@ -1,39 +1,23 @@
 import React from 'react';
 import { Avatar } from '@mui/material';
+import { styles } from './UserAvatar.styles';
 import PropTypes from 'prop-types';
 import bgFromString from '../../../utils/helpers/bgFromString';
 import checkContrast from '../../../utils/helpers/checkContrastColor';
 
 const UserAvatar = ({ userName, src, size }) => {
-  const styles = {
-    sm: {
-      width: '44px',
-      height: '44px',
-      fontSize: 16,
-      lineHeight: '28px',
-      letterSpacing: '0.15px',
-    },
-    l: {
-      width: '132px',
-      height: '132px',
-      fontSize: 48,
-      lineHeight: '56px',
-      letterSpacing: 0,
-    },
-  };
-
   const stringAvatar = (name) => {
     const BG_COLOR = bgFromString(name);
     return {
       sx: {
         backgroundColor: BG_COLOR,
+        borderRadius: styles[size].borderRadius,
         width: styles[size].width,
         height: styles[size].height,
-        borderRadius: '4px',
         fontSize: styles[size].fontSize,
         lineHeight: styles[size].lineHeight,
         letterSpacing: styles[size].letterSpacing,
-        fontWeight: 400,
+        fontWeight: styles[size].fontWeight,
         color: checkContrast(BG_COLOR),
       },
       children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
---
+++ src/components/UI/UserAvatar/UserAvatar.styles.js
@@ -0,0 +1,20 @@
+export const styles = {
+  sm: {
+    borderRadius: 1,
+    width: '44px',
+    height: '44px',
+    fontSize: 16,
+    lineHeight: '28px',
+    letterSpacing: '0.15px',
+    fontWeight: 400,
+  },
+  l: {
+    borderRadius: 1,
+    width: '132px',
+    height: '132px',
+    fontSize: 48,
+    lineHeight: '56px',
+    letterSpacing: 0,
+    fontWeight: 400,
+  },
+};
