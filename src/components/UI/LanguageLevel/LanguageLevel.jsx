f9a1367806a7823b2b982430b5227f1d1a75412f
BlackPuma9 <yuliiayavorska9@gmail.com>
2024-05-12 18:24:44 +0200

    `tobeDeleted` and `languageDeleteHandler` added to props for `modal`

--- src/components/UI/LanguageLevel/LanguageLevel.jsx
+++ src/components/UI/LanguageLevel/LanguageLevel.jsx
@@ -4,14 +4,21 @@ import { Box, IconButton, Typography } from '@mui/material';
 import PropTypes from 'prop-types';
 import CloseIcon from '@mui/icons-material/Close';
 
-const LanguageLevel = ({ language, level, tobeDeleted }) => {
+const LanguageLevel = ({ language, level, tobeDeleted, languageDeleteHandler }) => {
   return (
     <Box sx={styles.wrapper}>
       <Box sx={styles.language}>{language}</Box>
       <Typography variant='caption3' sx={styles.level}>
         {level}
       </Typography>
-      {tobeDeleted && <IconButton sx={styles.icon}><CloseIcon/></IconButton>}
+      {tobeDeleted &&
+        <IconButton
+          sx={styles.icon}
+          onClick={() => languageDeleteHandler(language)}
+        >
+          <CloseIcon/>
+        </IconButton>
+      }
     </Box>
   );
 };
@@ -20,6 +27,7 @@ LanguageLevel.propTypes = {
   language: PropTypes.string.isRequired,
   level: PropTypes.string.isRequired,
   tobeDeleted: PropTypes.bool,
+  languageDeleteHandler: PropTypes.func,
 };
 
 export default LanguageLevel;
--- src/components/UI/LanguagesList/LanguagesList.jsx
+++ src/components/UI/LanguagesList/LanguagesList.jsx
@@ -3,12 +3,18 @@ import PropTypes from 'prop-types';
 import LanguageLevel from '../LanguageLevel';
 
 const LanguagesList = ({ data }) => {
+  //Uncomment this block if you want to see how this would work in Modal
+  // const languageDeleteHandler = (language) => {
+  //   console.log(language);
+  // };
 
   return (
     <>
       {data.map(({id, language, level }) => (
-        <LanguageLevel key={id} language={language} level={level} tobeDeleted />
+        <LanguageLevel key={id} language={language} level={level} />
       ))}
+      {/*Uncomment to see for language component will be in modal*/}
+      {/*<LanguageLevel language={data[0].language} level={data[0].level} tobeDeleted languageDeleteHandler={languageDeleteHandler} />*/}
     </>
   );
 };
