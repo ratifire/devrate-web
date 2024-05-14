d4f1d266b6d728d5dc3fc3545f35bd7b0bb53886
BlackPuma9 <yuliiayavorska9@gmail.com>
2024-05-14 23:56:07 +0200

    Removed from `ErrorPage.jsx` demo of the `textarea` component.

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link, useRouteError } from 'react-router-dom';
-import TextAreaInput from '../components/Inputs/TextAreaInput';
 
 const ErrorPage = () => {
   const error = useRouteError();
@@ -14,13 +13,6 @@ const ErrorPage = () => {
         <hr />
         <Link to={`/`}>go to home</Link>
       </p>
-      <TextAreaInput
-        name='modal.editPage.personalInformation.title'
-        label='modal.editPage.personalInformation.title'
-        type="text"
-        // error
-        value="Кваліфікований Senior back-end розробник із 5-річним досвідом.  Працювала з базами даних: MySQL, MariaDB, MongoDB, MVC Framework (CakePHP). Робота з тестовою середовищем для PHP (PHPUnit). TV Archive, написання методів панелі управління / методів SOAP. Робота з тестовою с ередовищем для PHP (PHPUnit). TV Archive, написання методів панелі управління / методів SOAP. Робота з тестовою с ередовищем для PHP (PHPUnit). TV Archive, написання методів панелі управління / методів SOAP."
-        helperText="Here is the error" />
     </div>
   );
 };
