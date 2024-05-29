37936ad4a59656d1e66727032b709ae2b97bcc4b
SerjioBlack <serjioblack@gmail.com>
2024-05-29 17:52:07 +0300

    get achivments is working, fixed some structure and styles

--- src/components/ProfileComponents/ExperienceSection/Achievement/AchievementItem/AchievementItem.jsx
+++ src/components/ProfileComponents/ExperienceSection/Achievement/AchievementItem/AchievementItem.jsx
@@ -1,62 +1,49 @@
 import React from 'react';
-// import { useSelector } from 'react-redux';
-import { Box, IconButton, Typography } from '@mui/material';
+import PropTypes from 'prop-types';
+import { Box, IconButton, Link, Typography } from '@mui/material';
 import { ReactComponent as Star } from '../../../../../assets/icons/star.svg';
 import styles from './AchievementItem.styles.js';
 import DownloadIcon from '@mui/icons-material/Download';
-import { useFetchAchievementsQuery } from '../../../../../redux/services/achievementsApiSlice.js';
 
-const AchievementItem = () => {
-  // Extract userId from localStorage
-  const persistedAuth = localStorage.getItem('persist:auth');
-  const user = persistedAuth ? JSON.parse(persistedAuth).user : null;
-  const userId = user ? JSON.parse(user).data.id : null;
-
-  console.log('User ID:', userId);
-
-  const { data: achievements, error, isLoading } = useFetchAchievementsQuery(userId, {
-    skip: !userId, // Skip the query if userId is not available
-  });
-
-  if (!userId) {
-    return <Typography>Error: User not authenticated</Typography>;
-  }
-
-  if (isLoading) {
-    return <Typography>Loading...</Typography>;
-  }
-
-  if (error) {
-    return <Typography>Error: {error.message}</Typography>;
-  }
-
+const AchievementItem = ({ achievement }) => {
   return (
     <Box sx={styles.achievementItemContainer}>
-      {achievements?.map((achievement) => (
-        <Box key={achievement.id}>
-          <Box sx={styles.itemHeaderContainer}>
-            <Box sx={styles.logoTitleContainer}>
-              <Star width={46} height={38} />
-              <Box sx={styles.achievementTitleYearContainer}>
-                <Typography variant='h6' sx={styles.achievementTitle}>
-                  {achievement.summary}
-                </Typography>
-                <Typography variant='subtitle3' sx={styles.achievementYear}>
-                  (2020) {/* Assuming the year is constant or needs to be fetched from the achievement data */}
-                </Typography>
-              </Box>
+      <Box key={achievement.id} sx={styles.achievementContainer}>
+        <Box sx={styles.itemHeaderContainer}>
+          <Box sx={styles.logoTitleContainer}>
+            <Star width={46} height={38} />
+            <Box sx={styles.achievementTitleYearContainer}>
+              <Typography variant='h6' sx={styles.achievementTitle}>
+                {achievement.summary}
+              </Typography>
             </Box>
-            <IconButton sx={styles.icon}>
-              <DownloadIcon />
-            </IconButton>
           </Box>
-          <Typography variant='body' sx={styles.achievementItemText}>
-            {achievement.description}
-          </Typography>
+          <IconButton sx={styles.icon}>
+            <DownloadIcon />
+          </IconButton>
         </Box>
-      ))}
+        <Typography variant='body1' sx={styles.achievementItemText}>
+          {achievement.description}
+        </Typography>
+        {achievement.link && (
+          <Link href={achievement.link} target="_blank" sx={styles.link}>
+            <Typography variant='subtitle3'>
+              {achievement.link}
+            </Typography>
+          </Link>
+        )}
+      </Box>
     </Box>
   );
+};
+
+AchievementItem.propTypes = {
+  achievement: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    link: PropTypes.string,
+    description: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default AchievementItem;
