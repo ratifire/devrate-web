d15317af7b1ff3d9da78304151f261102ff27e73
BlackPuma9 <yuliiayavorska9@gmail.com>
2024-06-09 13:39:13 +0200

    Discarded the previously commented version of the code.

--- src/redux/services/api/apiSlice.js
+++ src/redux/services/api/apiSlice.js
@@ -1,19 +1,4 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
-//Previous version
-// const baseQuery = fetchBaseQuery({
-//   // eslint-disable-next-line
-//   baseUrl: process.env.REACT_APP_API_URL,
-//   credentials: 'include',
-// });
-//
-// export const apiSlice = createApi({
-//   baseQuery: baseQuery,
-//   // eslint-disable-next-line no-unused-vars
-//   endpoints: (builder) => ({}),
-// });
-
-// New Version
 import Cookies from 'js-cookie';
 import { logOut } from '../../auth/authSlice';
 
