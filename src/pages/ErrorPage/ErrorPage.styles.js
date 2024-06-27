4f2f6902f51f88c10380aaa249a57a169a29965c
Bohdan <boghdan.shimko@gmail.com>
2024-06-28 01:07:16 +0300

    refactor: ErrorPage

---
+++ src/pages/ErrorPage/ErrorPage.styles.js
@@ -0,0 +1,13 @@
+export const styles = {
+  box: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: '100vh',
+    padding: 2,
+  },
+  btn: {
+    width: 'auto',
+  },
+};
