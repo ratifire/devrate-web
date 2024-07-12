4369b8a1b167c151ef945def3f347af8b4145e0e
Bohdan <boghdan.shimko@gmail.com>
2024-07-12 17:53:00 +0300

    add: hard skills chart

--- src/components/SpecializationComponents/Statistics/HardSkillsChart/HardSkillsChart.jsx
+++ src/components/SpecializationComponents/Statistics/HardSkillsChart/HardSkillsChart.jsx
@@ -1,46 +1,36 @@
+/* eslint-disable */
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, CircularProgress, Typography } from '@mui/material';
 import { PolarAngleAxis, PolarGrid, PolarRadiusAxis, Radar, RadarChart, Tooltip } from 'recharts';
 
 import { styles } from './HardSkillsChart.style.js';
 import {useTranslation} from "react-i18next";
-
-const data = [
-  {
-    subject: 'React',
-    level: 120,
-    fullMark: 150,
-  },
-  {
-    subject: 'Java',
-    level: 98,
-    fullMark: 150,
-  },
-  {
-    subject: 'Next.JS',
-    level: 86,
-    fullMark: 150,
-  },
-  {
-    subject: 'MobX',
-    level: 99,
-    fullMark: 150,
-  },
-  {
-    subject: 'JS',
-    level: 85,
-    fullMark: 150,
-  },
-  {
-    subject: 'Node.js',
-    level: 65,
-    fullMark: 150,
-  },
-];
+import { useSelector } from 'react-redux';
+import {
+  useGetHardSkillsByMasteryIdQuery,
+  useGetMainMasteryBySpecializationIdQuery,
+  useGetSpecializationByUserIdQuery,
+} from '../../../../redux/specialization/specializationApiSlice';
 
 const HardSkillsChart = () => {
   const { t } = useTranslation();
-  
+  const { id: userId } = useSelector((state) => state.auth.user.data);
+  const { data: specializations, isLoading: isLoadingSpecializations } = useGetSpecializationByUserIdQuery(userId);
+  const specializationId = specializations?.[0]?.id
+  const { data: mainMastery, isLoading: isLoadingMainMastery } = useGetMainMasteryBySpecializationIdQuery(specializationId, { skip: !specializationId });
+  const {
+    data: skills = [],
+    isLoading: isLoadingSkills,
+    isError: isErrorSkills,
+  } = useGetHardSkillsByMasteryIdQuery({ userId, masteryId: mainMastery?.id }, { skip: !mainMastery?.id });
+
+  if (isLoadingSpecializations || isLoadingMainMastery || isLoadingSkills) {
+    return <CircularProgress />;
+  }
+
+  if (isErrorSkills) {
+    return <Typography variant='h6'>Something error...</Typography>;
+  }
   return (
     <Box sx={styles.hardSkillsChartContainer}>
       <Typography variant='subtitle2' sx={styles.title}>
@@ -49,12 +39,12 @@ const HardSkillsChart = () => {
       <Typography variant='subtitle3' sx={styles.text}>
          {t('specialization.statistics.hard_skills_chart_text')}
       </Typography>
-      <RadarChart outerRadius={130} width={264} height={264} data={data}>
+      <RadarChart outerRadius={130} width={264} height={264} data={skills}>
         <PolarGrid />
-        <PolarAngleAxis dataKey='subject' tick={false} />
-        <PolarRadiusAxis angle={30} domain={[0, 150]} />
+        <PolarAngleAxis dataKey='name' tick={false} />
+        <PolarRadiusAxis angle={30} domain={[0, 10]} />
         <Radar
-          dataKey='level'
+          dataKey='averageMark'
           stroke='url(#gradient2)'
           strokeWidth={3}
           fillOpacity={0.3}
