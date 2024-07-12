2b46307a94b283b0bbc484d3630b75d11f945370
Bohdan <boghdan.shimko@gmail.com>
2024-07-12 18:25:02 +0300

    fix: warning in Radar recharts and prettier formatter

--- src/components/SpecializationComponents/Statistics/HardSkillsChart/HardSkillsChart.jsx
+++ src/components/SpecializationComponents/Statistics/HardSkillsChart/HardSkillsChart.jsx
@@ -1,23 +1,24 @@
-/* eslint-disable */
-import React from 'react';
 import { Box, CircularProgress, Typography } from '@mui/material';
-import { PolarAngleAxis, PolarGrid, PolarRadiusAxis, Radar, RadarChart, Tooltip } from 'recharts';
-
-import { styles } from './HardSkillsChart.style.js';
-import {useTranslation} from "react-i18next";
+import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
+import { PolarAngleAxis, PolarGrid, PolarRadiusAxis, Radar, RadarChart, Tooltip } from 'recharts';
 import {
   useGetHardSkillsByMasteryIdQuery,
   useGetMainMasteryBySpecializationIdQuery,
   useGetSpecializationByUserIdQuery,
 } from '../../../../redux/specialization/specializationApiSlice';
+import { styles } from './HardSkillsChart.style.js';
 
 const HardSkillsChart = () => {
   const { t } = useTranslation();
   const { id: userId } = useSelector((state) => state.auth.user.data);
   const { data: specializations, isLoading: isLoadingSpecializations } = useGetSpecializationByUserIdQuery(userId);
-  const specializationId = specializations?.[0]?.id
-  const { data: mainMastery, isLoading: isLoadingMainMastery } = useGetMainMasteryBySpecializationIdQuery(specializationId, { skip: !specializationId });
+  const specializationId = specializations?.[0]?.id;
+  const { data: mainMastery, isLoading: isLoadingMainMastery } = useGetMainMasteryBySpecializationIdQuery(
+    specializationId,
+    { skip: !specializationId }
+  );
   const {
     data: skills = [],
     isLoading: isLoadingSkills,
@@ -34,10 +35,10 @@ const HardSkillsChart = () => {
   return (
     <Box sx={styles.hardSkillsChartContainer}>
       <Typography variant='subtitle2' sx={styles.title}>
-         {t('specialization.statistics.hard_skills_chart_title')}
+        {t('specialization.statistics.hard_skills_chart_title')}
       </Typography>
       <Typography variant='subtitle3' sx={styles.text}>
-         {t('specialization.statistics.hard_skills_chart_text')}
+        {t('specialization.statistics.hard_skills_chart_text')}
       </Typography>
       <RadarChart outerRadius={130} width={264} height={264} data={skills}>
         <PolarGrid />
@@ -48,12 +49,9 @@ const HardSkillsChart = () => {
           stroke='url(#gradient2)'
           strokeWidth={3}
           fillOpacity={0.3}
-          dot={<circle cx={1} cy={1} r={2} fill='#16FFB9' />}
+          dot={({ cx, cy, index }) => <circle key={index} cx={cx} cy={cy} r={2} fill='#16FFB9' />}
         />
-        <Tooltip
-          contentStyle={styles.tooltipContent}
-          labelStyle={styles.tooltipLabel}
-        />
+        <Tooltip contentStyle={styles.tooltipContent} labelStyle={styles.tooltipLabel} />
         <defs>
           <linearGradient id='gradient2' x1='0%' y1='0%' x2='100%' y2='0%'>
             <stop offset='0%' style={{ stopColor: '#16FFB9', stopOpacity: 1 }} />
--- src/components/SpecializationComponents/Statistics/LevelChart/LevelChart.jsx
+++ src/components/SpecializationComponents/Statistics/LevelChart/LevelChart.jsx
@@ -1,87 +1,88 @@
-import React from 'react';
 import { Box, CircularProgress, Typography } from '@mui/material';
-import {Gauge, gaugeClasses} from '@mui/x-charts';
-import {styles} from './LevelChart.styles.js';
-import {useTranslation} from "react-i18next";
+import { Gauge, gaugeClasses } from '@mui/x-charts';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import {
-	useGetHardSkillsByMasteryIdQuery,
-	useGetMainMasteryBySpecializationIdQuery,
-	useGetSpecializationByUserIdQuery,
+  useGetHardSkillsByMasteryIdQuery,
+  useGetMainMasteryBySpecializationIdQuery,
+  useGetSpecializationByUserIdQuery,
 } from '../../../../redux/specialization/specializationApiSlice';
+import { styles } from './LevelChart.styles.js';
 
 const LevelChart = () => {
-	const { t } = useTranslation();
-	const { id: userId } = useSelector((state) => state.auth.user.data);
-	const { data: specializations, isLoading: isLoadingSpecializations } = useGetSpecializationByUserIdQuery(userId);
-	const specializationId = specializations?.[0]?.id
-	const { data: mainMastery, isLoading: isLoadingMainMastery } = useGetMainMasteryBySpecializationIdQuery(specializationId, { skip: !specializationId });
-	const {
-		data: skills = [],
-		isLoading: isLoadingSkills,
-		isError: isErrorSkills,
-	} = useGetHardSkillsByMasteryIdQuery({ userId, masteryId: mainMastery?.id }, { skip: !mainMastery?.id });
+  const { t } = useTranslation();
+  const { id: userId } = useSelector((state) => state.auth.user.data);
+  const { data: specializations, isLoading: isLoadingSpecializations } = useGetSpecializationByUserIdQuery(userId);
+  const specializationId = specializations?.[0]?.id;
+  const { data: mainMastery, isLoading: isLoadingMainMastery } = useGetMainMasteryBySpecializationIdQuery(
+    specializationId,
+    { skip: !specializationId }
+  );
+  const {
+    data: skills = [],
+    isLoading: isLoadingSkills,
+    isError: isErrorSkills,
+  } = useGetHardSkillsByMasteryIdQuery({ userId, masteryId: mainMastery?.id }, { skip: !mainMastery?.id });
 
-	const averageMark = (skills.reduce((acc, skill) => acc + skill.averageMark, 0) / skills.length).toFixed(1) * 10 || 0
+  const averageMark = (skills.reduce((acc, skill) => acc + skill.averageMark, 0) / skills.length).toFixed(1) * 10 || 0;
 
-	if (isLoadingSpecializations || isLoadingMainMastery || isLoadingSkills) {
-		return <CircularProgress />;
-	}
+  if (isLoadingSpecializations || isLoadingMainMastery || isLoadingSkills) {
+    return <CircularProgress />;
+  }
 
-	if (isErrorSkills) {
-		return <Typography variant='h6'>Something error...</Typography>;
-	}
+  if (isErrorSkills) {
+    return <Typography variant='h6'>Something error...</Typography>;
+  }
 
-	return (
-		<Box sx={styles.levelChartContainer}>
-			<Typography variant='subtitle2' sx={styles.title}>
-				{t('specialization.statistics.level_chart_title')}
-			</Typography>
-			<Box sx={styles.chartContainer}>
-				<Box sx={{position: 'relative'}}>
-					<Box
-						sx={styles.chartWrapper}
-					>
-						<Gauge
-							value={averageMark}
-							startAngle={-90}
-							endAngle={90}
-							innerRadius='80%'
-							outerRadius='100%'
-							cornerRadius='90%'
-							sx={{
-								padding: 0,
-								height: '150px',
-								'.MuiGauge-valueArc': {
-									fill: 'url(#gradient)',
-									strokeWidth: 12,
-								},
-								[`& .${gaugeClasses.valueText}`]: {
-									fontSize: 48,
-									transform: 'translate(0px, -30px)',
-								},
-							}}
-							text={({value}) => `${value} %`}
-						>
-							<defs>
-								<linearGradient id='gradient' x1='0%' y1='0%' x2='100%' y2='0%'>
-									<stop offset='0%' style={{stopColor: '#EFE6FD', stopOpacity: 1}}/>
-									<stop offset='50%' style={{stopColor: '#B78AF7', stopOpacity: 1}}/>
-									<stop offset='100%' style={{stopColor: '#8133F1', stopOpacity: 1}}/>
-								</linearGradient>
-							</defs>
-						</Gauge>
-					</Box>
-				</Box>
-				<Typography variant='caption' sx={styles.rightCaption}>
-					{t('specialization.statistics.level_chart_left_caption')}
-				</Typography>
-				<Typography variant='caption' sx={styles.leftCaption}>
-					{t('specialization.statistics.level_chart_right_caption')}
-				</Typography>
-			</Box>
-		</Box>
-	);
+  return (
+    <Box sx={styles.levelChartContainer}>
+      <Typography variant='subtitle2' sx={styles.title}>
+        {t('specialization.statistics.level_chart_title')}
+      </Typography>
+      <Box sx={styles.chartContainer}>
+        <Box sx={{ position: 'relative' }}>
+          <Box sx={styles.chartWrapper}>
+            <Gauge
+              value={averageMark}
+              startAngle={-90}
+              endAngle={90}
+              innerRadius='80%'
+              outerRadius='100%'
+              cornerRadius='90%'
+              sx={{
+                padding: 0,
+                height: '150px',
+                '.MuiGauge-valueArc': {
+                  fill: 'url(#gradient)',
+                  strokeWidth: 12,
+                },
+                [`& .${gaugeClasses.valueText}`]: {
+                  fontSize: 48,
+                  transform: 'translate(0px, -30px)',
+                },
+              }}
+              text={({ value }) => `${value} %`}
+            >
+              <defs>
+                <linearGradient id='gradient' x1='0%' y1='0%' x2='100%' y2='0%'>
+                  <stop offset='0%' style={{ stopColor: '#EFE6FD', stopOpacity: 1 }} />
+                  <stop offset='50%' style={{ stopColor: '#B78AF7', stopOpacity: 1 }} />
+                  <stop offset='100%' style={{ stopColor: '#8133F1', stopOpacity: 1 }} />
+                </linearGradient>
+              </defs>
+            </Gauge>
+          </Box>
+        </Box>
+        <Typography variant='caption' sx={styles.rightCaption}>
+          {t('specialization.statistics.level_chart_left_caption')}
+        </Typography>
+        <Typography variant='caption' sx={styles.leftCaption}>
+          {t('specialization.statistics.level_chart_right_caption')}
+        </Typography>
+      </Box>
+    </Box>
+  );
 };
 
 export default LevelChart;
