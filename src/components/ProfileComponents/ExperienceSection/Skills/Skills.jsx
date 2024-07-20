3106b6e36c5e636c720f5abe1360a8dd8f94e326
VladyslavDykyi <vladislavdykyi33@gmail.com>
2024-07-20 15:36:25 +0300

    section skills

--- src/components/ProfileComponents/ExperienceSection/Skills/Skills.jsx
+++ src/components/ProfileComponents/ExperienceSection/Skills/Skills.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 const Skills = () => {
-	return (
-		<div>Skills</div>
-	)
-}
-export default Skills
+  return (
+    <div>
+      <div>
+        <h2>Frontend Developer</h2>
+        <p>Level Junior</p>
+        <div>
+          <div>
+            <div></div>
+            <p></p>
+          </div>
+        </div>
+      </div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div>
+    </div>
+  );
+};
+export default Skills;
