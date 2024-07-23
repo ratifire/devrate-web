d28d8d25917935ac27cb7341484ebd4ada3d2f75
SerjioBlack <serjioblack@gmail.com>
2024-07-23 10:22:02 +0300

    special characters now cant be used in email

--- src/components/AuthModals/ResetPasswordModal/CheckEmail/CheckEmailSchema.js
+++ src/components/AuthModals/ResetPasswordModal/CheckEmail/CheckEmailSchema.js
@@ -2,6 +2,10 @@ import * as Yup from 'yup';
 
 export const CheckEmailSchema = Yup.object().shape({
   email: Yup.string()
+  .matches(
+    /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    'modal.checkEmailResetPassword.email_invalid'
+  )
     .email('modal.checkEmailResetPassword.email_invalid')
     .required('modal.checkEmailResetPassword.required'),
 });
