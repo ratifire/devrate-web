b9f106433919207ec6baa69a720f98d4ef33a946
SerjioBlack <serjioblack@gmail.com>
2024-07-31 16:21:36 +0300

    all sections are set to refresh progress bar progress

---
+++ src/components/ProfileComponents/BaseUserInfo/useProfileProgress.js
@@ -0,0 +1,63 @@
+// useProfileProgress.js
+import { useState, useEffect } from 'react';
+import { useGetWorkExperienceByUserIdQuery } from '../../../redux/workExperience/workExperienceApiSlice';
+import { useFetchAchievementsQuery } from '../../../redux/services/achievementsApiSlice';
+import { useGetEducationByUserIdQuery } from '../../../redux/services/educationApiSlice';
+import { useGetSpecializationByUserIdQuery } from '../../../redux/specialization/specializationApiSlice';
+import { useGetUserContactsQuery } from '../../../redux/user/contacts/contactsApiSlice';
+import { useGetPersonalUserQuery } from '../../../redux/user/personal/personalApiSlice';
+import { useGetLanguageUserQuery } from '../../../redux/user/language/languageApiSlice';
+import { useGetAvatarUserQuery } from '../../../redux/user/avatar/avatarApiSlice';
+
+export const useProfileProgress = (userId) => {
+  const [progress, setProgress] = useState(5);
+
+  const { data: workExperience } = useGetWorkExperienceByUserIdQuery(userId);
+  const { data: achievements } = useFetchAchievementsQuery(userId, { skip: !userId });
+  const { data: educations } = useGetEducationByUserIdQuery(userId);
+  const { data: specializations } = useGetSpecializationByUserIdQuery(userId);
+  const { data: userContacts } = useGetUserContactsQuery(userId);
+  const { data: personalData } = useGetPersonalUserQuery(userId);
+  const { data: languages } = useGetLanguageUserQuery(userId);
+  const { data: avatarData } = useGetAvatarUserQuery(userId);
+
+  useEffect(() => {
+    let newProgress = 10;
+
+    if (workExperience && workExperience.length > 0) {
+      newProgress += 10;
+    }
+
+    if (achievements && achievements.length > 0) {
+      newProgress += 10;
+    }
+
+    if (educations && educations.length > 0) {
+      newProgress += 10;
+    }
+
+    if (specializations && specializations.length > 0) {
+      newProgress += 20;
+    }
+
+    if (userContacts && Object.values(userContacts).some(value => value)) {
+      newProgress += 10;
+    }
+
+    if (personalData && personalData.description) {
+      newProgress += 10;
+    }
+
+    if (languages && languages.length > 0) {
+      newProgress += 10;
+    }
+
+    if (avatarData && avatarData.userPicture) {
+      newProgress += 10;
+    }
+
+    setProgress(newProgress);
+  }, [workExperience, achievements, educations, specializations, userContacts, personalData, languages, avatarData]);
+
+  return progress;
+};
