c8b90e6823c2fc394c43559c85c04452505f13d2
Bohdan <boghdan.shimko@gmail.com>
2024-08-26 01:28:08 +0300

    prettier

--- src/components/SpecializationComponents/SoftSkills/SoftSkills.jsx
+++ src/components/SpecializationComponents/SoftSkills/SoftSkills.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import { Box, CircularProgress, IconButton, Typography } from '@mui/material';
-import { styles } from './SoftSkills.styles';
 import EditIcon from '@mui/icons-material/Edit';
-import Item from './Item';
+import { Box, CircularProgress, IconButton, Typography } from '@mui/material';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import { openModal } from '../../../redux/modal/modalSlice';
 import {
   useGetMasteriesBySpecializationIdQuery,
-  useGetSoftSkillsQuery, useGetSpecializationByUserIdQuery,
+  useGetSoftSkillsQuery,
+  useGetSpecializationByUserIdQuery,
 } from '../../../redux/specialization/specializationApiSlice';
+import Item from './Item';
+import { styles } from './SoftSkills.styles';
 
 const SoftSkills = () => {
   const dispatch = useDispatch();
   const { t } = useTranslation();
   const { id: userId } = useSelector((state) => state.auth.user.data);
   const activeMastery = useSelector((state) => state.activeMastery.activeMastery);
+
   const {
     data: specializations,
     isLoading: isLoadingSpecializations,
@@ -23,29 +25,35 @@ const SoftSkills = () => {
   } = useGetSpecializationByUserIdQuery(userId, {
     skip: !userId,
   });
+
   const specializationId = specializations?.[0]?.id;
+
   const {
     data: masteries,
     isLoading: isLoadingMasteries,
     isError: isErrorMasteries,
   } = useGetMasteriesBySpecializationIdQuery(specializationId, { skip: !specializationId });
+
   const selectMastery = masteries?.find(
     (mastery) => mastery.level && mastery.level.toUpperCase() === activeMastery.toUpperCase()
   );
+
   const masteryId = selectMastery?.id;
+
   const {
     data: softSkills,
     isLoading: isLoadingSoftSkill,
-    isError: isErrorSoftSkill
+    isError: isErrorSoftSkill,
   } = useGetSoftSkillsQuery(masteryId, { skip: !masteryId });
 
   const handleModalOpen = () => {
     dispatch(openModal({ modalName: 'openSoftSkillsModal' }));
   };
 
-  const averageMarkNumber = softSkills?.length > 0 ?
-    (softSkills?.reduce((acc, skill) => acc + skill.averageMark, 0) / softSkills?.length).toFixed(1)
-    : '0'
+  const averageMarkNumber =
+    softSkills?.length > 0
+      ? (softSkills?.reduce((acc, skill) => acc + skill.averageMark, 0) / softSkills?.length).toFixed(1)
+      : '0';
 
   const isLoading = isLoadingSpecializations || isLoadingSoftSkill || isLoadingMasteries;
   const isError = isErrorSoftSkill || isErrorLoadingSpecializations || isErrorMasteries;
@@ -58,12 +66,11 @@ const SoftSkills = () => {
     return <Typography variant='h6'>{t('specialisation.hardSkills.error')}</Typography>;
   }
 
-
   return (
     <Box sx={styles.wrapper}>
       <Box sx={styles.title}>
-        <Typography variant="h6">{t('specialization.softSkills.title')}</Typography>
-        <IconButton sx={styles.btnIcon} aria-label="Edit user information" onClick={handleModalOpen}>
+        <Typography variant='h6'>{t('specialization.softSkills.title')}</Typography>
+        <IconButton sx={styles.btnIcon} aria-label='Edit user information' onClick={handleModalOpen}>
           <EditIcon />
         </IconButton>
       </Box>
@@ -75,8 +82,8 @@ const SoftSkills = () => {
       </Box>
 
       <Box sx={styles.markWrapper}>
-        <Typography variant="h6">{t('specialization.hardSkills.averageMark')}</Typography>
-        <Typography sx={styles.mark} variant="h6">{`${averageMarkNumber || '0'}/10`}</Typography>
+        <Typography variant='h6'>{t('specialization.hardSkills.averageMark')}</Typography>
+        <Typography sx={styles.mark} variant='h6'>{`${averageMarkNumber || '0'}/10`}</Typography>
       </Box>
     </Box>
   );
