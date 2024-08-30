976654b4df39131510df31faeeb5ae333318c7af
BlackPuma9 <yuliiayavorska9@gmail.com>
2024-08-30 22:36:19 +0200

    Refactor: changed div to box

--- src/components/PageComponents/Calendar/Sidebar/Sidebar.jsx
+++ src/components/PageComponents/Calendar/Sidebar/Sidebar.jsx
@@ -6,6 +6,7 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateCalendar } from '@mui/x-date-pickers';
 import { Settings } from 'luxon';
 import { AdapterLuxon } from '@mui/x-date-pickers/AdapterLuxon';
+import { Box } from '@mui/material';
 
 Settings.defaultWeekSettings = {
   firstDay: 1, // Set the first day of the week
@@ -16,24 +17,24 @@ Settings.defaultWeekSettings = {
 // export default function Sidebar({ weekendsVisible, handleWeekendsToggle, currentEvents }) {
 export default function Sidebar({ currentEvents }) {
   return (
-    <div style={styles.Sidebar}>
-      <div className='demo-app-sidebar-section'>
+    <Box style={styles.Sidebar}>
+      <Box className='demo-app-sidebar-section'>
         <LocalizationProvider dateAdapter={AdapterLuxon}>
           <DateCalendar />
         </LocalizationProvider>
-      </div>
+      </Box>
       {/*<div style={styles.SidebarSection} className='demo-app-sidebar-section'>*/}
       {/*  <label>*/}
       {/*    <input type='checkbox' checked={weekendsVisible} onChange={handleWeekendsToggle}></input>*/}
       {/*    toggle weekends*/}
       {/*  </label>*/}
       {/*</div>*/}
-      <div style={styles.SidebarSection} className='demo-app-sidebar-section'>
+      <Box style={styles.SidebarSection} className='demo-app-sidebar-section'>
         {currentEvents.map((event) => (
           <SidebarEvent key={event.id} event={event} />
         ))}
-      </div>
-    </div>
+      </Box>
+    </Box>
   );
 }
 
