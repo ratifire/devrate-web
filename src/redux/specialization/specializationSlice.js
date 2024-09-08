55db6a79e30c80f8431babfb78f28a6d53c4e096
Bohdan <boghdan.shimko@gmail.com>
2024-09-08 22:00:34 +0300

    add: activeSpecialization and mainSpecialization in specializationSlice.js

--- src/redux/specialization/specializationSlice.js
+++ src/redux/specialization/specializationSlice.js
@@ -3,6 +3,8 @@ import { createSlice } from '@reduxjs/toolkit';
 const specializationSlice = createSlice({
   name: 'specialization',
   initialState: {
+    activeSpecialization: null,
+    mainSpecialization: null,
     selectedSpecialization: null,
     mainMastery: null,
   },
@@ -13,8 +15,20 @@ const specializationSlice = createSlice({
     setMainMastery: (state, { payload }) => {
       state.mainMastery = { ...payload };
     },
+    // =============================================================
+    setActiveSpecialization: (state, { payload }) => {
+      state.activeSpecialization = payload
+    },
+    setMainSpecializations: (state, { payload }) => {
+      if (Array.isArray(payload)) {
+        state.mainSpecialization =  payload.find((spec) => spec.main);
+        return
+      }
+
+      state.mainSpecialization = payload;
+    },
   },
 });
 
-export const { setSelectedSpecialization, setMainMastery } = specializationSlice.actions;
+export const { setSelectedSpecialization, setMainMastery, setActiveSpecialization, setMainSpecializations } = specializationSlice.actions;
 export default specializationSlice.reducer;
