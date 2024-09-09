bd414db971955e90dc7fcfcc4ea026e410251dc0
Bohdan <boghdan.shimko@gmail.com>
2024-09-09 16:29:47 +0300

    fix: InterviewChart.jsx

--- src/components/PageComponents/SpecializationComponents/Statistics/InteviewChart/InterviewChart.jsx
+++ src/components/PageComponents/SpecializationComponents/Statistics/InteviewChart/InterviewChart.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import { Box, CircularProgress, MenuItem, Select, Typography } from '@mui/material';
 import React, { useMemo } from 'react';
@@ -16,8 +15,6 @@ import { styles } from './InterviewChart.style';
 
 const InterviewChart = () => {
   const { id: userId } = useSelector((state) => state.auth.user.data);
-  const foo = useSelector((state) => state.auth.user.data);
-  // console.log(foo);
   const { to, from } = useMemo(() => getCurrentAndLastMonths(), []);
   const { data, isLoading, isError } = useGetInterviewSummariesStatisticQuery({ userId, from, to }, { skip: !userId });
   const { t } = useTranslation();
