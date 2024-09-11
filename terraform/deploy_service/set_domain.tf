49a2932ea8cf9f6337697de3bb60512391736131
NewEnergyCode <matveychuk.m@gmail.com>
2024-09-11 21:45:13 +0300

    setup build_and_deploy_new_image.yml workflow change env

--- terraform/deploy_service/set_domain.tf
+++ terraform/deploy_service/set_domain.tf
@@ -15,8 +15,20 @@ resource "aws_route53_record" "front_a_record" {
 resource "aws_acm_certificate" "devrate_cert" {
   domain_name       = "devrate.org"
   validation_method = "DNS"
+
+  subject_alternative_names = [
+    "*.devrate.org"
+  ]
 }
 
+resource "aws_route53_record" "devrate_cert_validation" {
+  zone_id = data.aws_route53_zone.front_dns_zone.zone_id
+
+  name    = aws_acm_certificate.devrate_cert.domain_validation_options[0].resource_record_name
+  type    = aws_acm_certificate.devrate_cert.domain_validation_options[0].resource_record_type
+  ttl     = 60
+  records = [aws_acm_certificate.devrate_cert.domain_validation_options[0].resource_record_value]
+}
 
 resource "aws_lb_listener" "https_ecs_listener" {
   load_balancer_arn = aws_lb.front_ecs_alb.arn
