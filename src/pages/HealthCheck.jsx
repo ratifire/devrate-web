4000e2d30d5bb84ec07c732463150bb0e1076745
NewEnergyCode <matveychuk.m@gmail.com>
2024-09-11 16:14:12 +0300

    setup build_and_deploy_new_image.yml workflow change env

--- src/pages/HealthCheck.jsx
+++ src/pages/HealthCheck.jsx
@@ -2,16 +2,37 @@ import React, { useEffect, useState } from 'react';
 
 const HealthCheck = () => {
   const [status, setStatus] = useState('Checking...');
+  const [health, setHealth] = useState(false);
 
   useEffect(() => {
-    // You can add more checks here if you want to check external services (e.g., API health)
-    setStatus('App is Healthy');
+    // Функция для проверки здоровья API или сервиса
+    const checkHealth = async () => {
+      try {
+        // Здесь можно сделать запрос к бэкенду или другому API для проверки
+        const response = await fetch('/health'); // Пример запроса
+        if (response.ok) {
+          setHealth(true);
+          setStatus('healthy');
+        } else {
+          setHealth(false);
+          setStatus('unhealthy');
+        }
+      } catch (error) {
+        setHealth(false);
+        setStatus('unhealthy');
+      }
+    };
+
+    // Вызов проверки здоровья при монтировании компонента
+    checkHealth();
   }, []);
 
   return (
     <div>
       <h1>Health Check</h1>
       <p>Status: {status}</p>
+      {/* AWS ALB проверяет только содержимое страницы, поэтому тут просто выводим текст */}
+      <p>{health ? 'healthy' : 'unhealthy'}</p>
     </div>
   );
 };
