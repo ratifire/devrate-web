493cfcee22800d61034d210ea29f8743b7993677
NewEnergyCode <matveychuk.m@gmail.com>
2024-09-11 21:12:23 +0300

    setup build_and_deploy_new_image.yml workflow change env

--- src/utils/hooks/useSocket.js
+++ src/utils/hooks/useSocket.js
@@ -4,7 +4,7 @@ export function useSocket(url, onOpen) {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const ws = new WebSocket(`wss://server.devrate.org${url}`);
+    const ws = new WebSocket(`wss://server.devrate.org/ws`);
     ws.addEventListener('open', event => {
       setSocket(event.target);
       onOpen(event);
