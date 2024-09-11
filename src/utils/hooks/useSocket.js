95b7df7d21e43cfda09603e57d5a1cf29acd9baf
NewEnergyCode <matveychuk.m@gmail.com>
2024-09-11 15:34:33 +0300

    setup build_and_deploy_new_image.yml workflow change env

--- src/utils/hooks/useSocket.js
+++ src/utils/hooks/useSocket.js
@@ -4,7 +4,7 @@ export function useSocket(url, onOpen) {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const ws = new WebSocket(`${process.env.REACT_APP_WS_URL}${url}`);
+    const ws = new WebSocket(`${process.env.REACT_APP_WS_URL}`);
     ws.addEventListener('open', event => {
       setSocket(event.target);
       onOpen(event);
