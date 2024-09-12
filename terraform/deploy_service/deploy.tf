b2465f78fb6ec0704db37d026da9823a24dc84d1
NewEnergyCode <matveychuk.m@gmail.com>
2024-09-12 14:24:57 +0300

    setup build_and_deploy_new_image.yml workflow change env

--- terraform/deploy_service/deploy.tf
+++ terraform/deploy_service/deploy.tf
@@ -138,6 +138,11 @@ resource "aws_lb" "front_ecs_alb" {
   load_balancer_type = "application"
   security_groups    = [data.aws_security_group.vpc_frontend_security_group.id]
   subnets            = data.aws_subnets.default_subnets.ids
+  access_logs {
+    enabled = true
+    bucket  = "logs-prod"
+    prefix  = "alb/alb-prod"
+  }
 }
 
 
