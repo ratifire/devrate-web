ccaa44c5a945f53aa82d69587c7d168fddd15614
Bohdan <boghdan.shimko@gmail.com>
2024-09-13 22:28:59 +0300

    fix

--- src/components/UI/SpecializationSkills/SpecializationSkills.jsx
+++ src/components/UI/SpecializationSkills/SpecializationSkills.jsx
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types';
 import { SpecializationSkillsItem } from './SpecializationSkillsItem';
 
 const SpecializationSkills = ({ isLoading, isError, skills, averageMark, openModal, errorTitle, title, subTitle }) => {
-  const { t } = useTranslation();
+  const { t } = useTranslation()
 
   if (isLoading) {
     return <CircularProgress />;
