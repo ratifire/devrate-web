328b4ef5e434f89aad7dff382710f18ed34dc14c
NewEnergyCode <matveychuk.m@gmail.com>
2024-09-23 21:39:02 +0300

    added environment variables into build_and_deploy_new_image.yml

--- terraform/deploy_service/load_balancer_setup.tf
+++ terraform/deploy_service/load_balancer_setup.tf
@@ -12,10 +12,10 @@ resource "aws_lb" "front_ecs_alb" {
 
 
 resource "aws_lb_target_group" "http_ecs_tg_front" {
-  name     = "http-ecs-tg-front"
-  port     = var.front_port
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpcs.all_vpcs.ids[0]
+  name                 = "http-ecs-tg-front"
+  port                 = var.front_port
+  protocol             = "HTTP"
+  vpc_id               = data.aws_vpcs.all_vpcs.ids[0]
   deregistration_delay = "120"
   stickiness {
     type            = "lb_cookie"
