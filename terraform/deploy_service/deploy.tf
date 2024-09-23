36ef7f06be015f4d2b6b14cd752196104c07b23d
NewEnergyCode <matveychuk.m@gmail.com>
2024-09-23 16:50:52 +0300

    added instance into service

--- terraform/deploy_service/deploy.tf
+++ terraform/deploy_service/deploy.tf
@@ -108,7 +108,7 @@ resource "aws_ecs_service" "front_services" {
   cluster                            = var.front_cluster_name
   task_definition                    = aws_ecs_task_definition.task_definition_front.arn
   scheduling_strategy                = "REPLICA"
-  desired_count                      = 1
+  desired_count                      = 2
   force_new_deployment               = true
   deployment_minimum_healthy_percent = 100
   deployment_maximum_percent         = 200
