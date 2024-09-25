d19af060824890a8635ed903ede718eed5030fc9
Bohdan <boghdan.shimko@gmail.com>
2024-09-25 20:33:45 +0300

    add: scroll bar in SpecializationSkills.styles.js

--- src/components/UI/Specialization/SpecializationSkills/SpecializationSkills.styles.js
+++ src/components/UI/Specialization/SpecializationSkills/SpecializationSkills.styles.js
@@ -22,41 +22,21 @@ export const styles = {
     marginBottom: theme.spacing(4),
   }),
   skillsContainer: {
-    height: 'auto',
+    maxHeight: '312px',
+    overflowY: 'auto',
+    paddingRight: '10px',
+    '&::-webkit-scrollbar': {
+      width: 10,
+    },
+    '&::-webkit-scrollbar-track': (theme) => ({
+      backgroundColor: theme.palette.neutral['600'],
+      borderRadius: 8,
+    }),
+    '&::-webkit-scrollbar-thumb': (theme) => ({
+      borderRadius: 6,
+      backgroundColor: theme.palette.common.white,
+    }),
   },
-  skillContainer: (theme) => ({
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    '>div:first-of-type': {
-      flex: '1 2 calc(100% - 130px)',
-      '>h6': {
-        flex: '1 2 max(266px)',
-        maxWidth: '266px',
-        width: '10vw',
-        textOverflow: 'ellipsis',
-        overflow: 'hidden',
-      },
-    },
-    '>div:last-of-type': {
-      flex: '1 2 122px',
-      marginLeft: theme.spacing(2),
-      gridGap: theme.spacing(2),
-      '>h6': {
-        minWidth: '50px',
-        textAlign: 'right',
-      },
-    },
-  }),
-  iconWrapper: (theme) => ({
-    display: 'flex',
-    alignItems: 'center',
-    gap: theme.spacing(2),
-  }),
-  divider: (theme) => ({
-    marginY: theme.spacing(2),
-    backgroundColor: theme.palette.background.level3,
-  }),
   markWrapper: (theme) => ({
     display: 'flex',
     justifyContent: 'space-between',
@@ -66,13 +46,4 @@ export const styles = {
   mark: (theme) => ({
     color: theme.palette.action.active,
   }),
-  iconCircle: {
-    fill: '#252527',
-  },
-  arrowDownIcon: {
-    fill: '#ED0E0E',
-  },
-  arrowUpIcon: {
-    fill: '#64FF2E',
-  },
 };
