623f2c671bdd47627d689d4264acc6d6f871c7b8
Bohdan <boghdan.shimko@gmail.com>
2024-10-07 21:08:46 +0300

    save

---
+++ src/pages/TestPage.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import React from 'react';
+import { InterviewerFeedback, RespondentFeedback } from '../components/ModalsComponents/FeedbackModal';
+
+const TestPage = () => {
+  return (
+    <>
+      {/*<InterviewerFeedback/>*/}
+      <RespondentFeedback/>
+    </>
+  )
+}
+
+export default TestPage;
