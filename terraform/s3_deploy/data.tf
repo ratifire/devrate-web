2e18d6002829a7cd8041f6c380862a7193b122f1
NewEnergyCode <matveychuk.m@gmail.com>
2024-10-14 21:09:29 +0300

    test s3 deploying

--- terraform/s3_deploy/data.tf
+++ terraform/s3_deploy/data.tf
@@ -23,14 +23,6 @@ resource "aws_s3_bucket_ownership_controls" "s3_front_ownership" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "s3_front_site_pb" {
-  bucket = data.aws_s3_bucket.s3_front_site.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
 
 resource "aws_s3_bucket_policy" "s3_front_policy" {
   bucket = data.aws_s3_bucket.s3_front_site.id
