494dbd095bf668388c2c9b4a8a5e31fd00aafc14
Bohdan <boghdan.shimko@gmail.com>
2024-10-16 21:10:21 +0300

    refactor: FeedbackModal.jsx

--- src/components/ModalsComponents/FeedbackModal/FeedbackModal.jsx
+++ src/components/ModalsComponents/FeedbackModal/FeedbackModal.jsx
@@ -1,33 +1,24 @@
-/* eslint-disable */
 import React from 'react';
 import ModalLayoutProfile from '../../../layouts/ModalLayoutProfile';
 import { useDispatch, useSelector } from 'react-redux';
 import { closeFeedbackModal } from '../../../redux/feedback/feedbackModalSlice';
 import { useGetInterviewByIdQuery } from '../../../redux/feedback/interviewApiSlice';
 import { RenderRoleModal } from './components/RenderRoleModal';
-import { FeedbackModalSkeleton } from '../../UI/Skeleton';
 
 const FeedbackModal = () => {
   const dispatch = useDispatch();
   const { open, feedbackId } = useSelector((state) => state.feedback);
-  const { data = [], isError, isFetching } = useGetInterviewByIdQuery({id: feedbackId}, { skip: !feedbackId });
+  const { data, isError, isFetching } = useGetInterviewByIdQuery({id: feedbackId}, { skip: !feedbackId });
+
   const handleCloseModal = () => {
     dispatch(closeFeedbackModal());
   }
 
-  if (isFetching) {
-    return (
-      <ModalLayoutProfile setOpen={handleCloseModal} open={open}>
-        <FeedbackModalSkeleton />
-      </ModalLayoutProfile>
-    )
-  }
+  const role = data?.participant?.role || '';
 
-  const { interviewStartTime, participant: { id, name, role, status, surname }, skills } = data;
-  console.log(role, id);
   return (
     <ModalLayoutProfile setOpen={handleCloseModal} open={open}>
-      <RenderRoleModal role={role} data={data} />
+      <RenderRoleModal isFetching={isFetching} isError={isError} role={role} data={data} />
     </ModalLayoutProfile>
   )
 }
--- src/components/ModalsComponents/FeedbackModal/components/RenderRoleModal/RenderRoleModal.jsx
+++ src/components/ModalsComponents/FeedbackModal/components/RenderRoleModal/RenderRoleModal.jsx
@@ -2,18 +2,30 @@ import PropTypes from 'prop-types';
 import { CandidateFeedback } from '../CandidateFeedback';
 import React from 'react';
 import { InterviewerFeedback } from '../InterviewerFeedback';
+import { FeedbackModalSkeleton } from '../../../../UI/Skeleton';
+import { ErrorComponent } from '../../../../UI/Exceptions';
 
-const RenderRoleModal = ({ role, data }) => {
+const RenderRoleModal = ({ role, data, isFetching, isError }) => {
+  if (isFetching) {
+    return <FeedbackModalSkeleton />;
+  }
+
+  if (isError) {
+    return <ErrorComponent/>;
+  }
+
   const ModalElement = {
     CANDIDATE: CandidateFeedback,
     INTERVIEWER: InterviewerFeedback,
-  }[role]
+  }[role] ?? ErrorComponent;
 
   return <ModalElement data={data} />;
 }
 
 RenderRoleModal.propTypes = {
   role: PropTypes.string.isRequired,
+  isFetching: PropTypes.bool.isRequired,
+  isError: PropTypes.bool.isRequired,
   data: PropTypes.shape({
     interviewStartTime: PropTypes.string.isRequired,
     participant: PropTypes.shape({
