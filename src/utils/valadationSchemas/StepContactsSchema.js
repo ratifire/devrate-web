f14a70c73a6667d0a7a695f0540bef48a153ca81
BlackPuma9 <yuliiayavorska9@gmail.com>
2024-10-25 20:43:18 +0200

    New feature: removed commented code and changed error message

--- src/utils/valadationSchemas/StepContactsSchema.js
+++ src/utils/valadationSchemas/StepContactsSchema.js
@@ -7,12 +7,12 @@ const behanceRegex = /^https:\/\/www\.behance\.net\/[a-zA-Z0-9-]+\/?$/;
 const telegramRegex = /^https:\/\/t\.me\/[a-zA-Z0-9_]+\/?$/;
 
 export const StepContactsSchema = Yup.object().shape({
-  telegram: Yup.string().matches(telegramRegex, 'please type correct URL, username').optional(),
-  linkedIn: Yup.string().matches(linkedinRegex, 'please type correct URL').optional(),
-  gitHub: Yup.string().matches(githubRegex, 'please type correct URL').optional(),
-  behance: Yup.string().matches(behanceRegex, 'please type correct URL').optional(),
+  telegram: Yup.string().matches(telegramRegex, 'Please enter correct URL').optional(),
+  linkedIn: Yup.string().matches(linkedinRegex, 'Please enter correct URL').optional(),
+  gitHub: Yup.string().matches(githubRegex, 'Please enter correct URL').optional(),
+  behance: Yup.string().matches(behanceRegex, 'Please enter correct URL').optional(),
   mail: emailValidationSchema,
   phone: Yup.string()
-    .matches(/^\+?[1-9]\d{1,14}$/, 'please type correct Phone number')
+    .matches(/^\+?[1-9]\d{1,14}$/, 'Please enter correct phone number')
     .optional(),
 });
