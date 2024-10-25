d7644415b92259eb8ec12bfb20d95330862ccd55
BlackPuma9 <yuliiayavorska9@gmail.com>
2024-10-25 20:39:29 +0200

    New feature: added `EmailValidationSchema.js` and exported to `1. CheckResetEmailSchema.js, 2. StepContactsSchema.js, 3. ModalUserInfoSchema.js`

--- src/utils/valadationSchemas/RegistrationSchema.js
+++ src/utils/valadationSchemas/RegistrationSchema.js
@@ -1,40 +1,15 @@
 import * as Yup from 'yup';
+import { emailValidationSchema } from './EmailValidationSchema';
 
 export const RegistrationSchema = Yup.object().shape({
-  email: Yup.string()
-    .matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 'modal.registration.email_invalid')
-    .email('modal.registration.email_invalid')
-    .required('modal.registration.required')
-    .test('custom-email', 'modal.registration.email_invalid', function(value) {
-      if ( !value) return false;
-      if (value.length > 254) return false;
-      
-      const [localPart, domainPart] = value.split('@');
-      if ( !localPart || !domainPart) return false;
-      if (localPart.length > 64 || domainPart.length > 255) return false;
-      
-      const domainParts = domainPart.split('.');
-      if (domainParts.some(part => part.length > 63)) return false;
-      
-      const localPartRegex = /^[a-zA-Z0-9._%+-]+$/;
-      if ( !localPartRegex.test(localPart)) return false;
-      
-      const domainPartRegex = /^[a-zA-Z0-9.-]+$/;
-      if ( !domainPartRegex.test(domainPart)) return false;
-      
-      const domainPattern = /^[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/;
-      if ( !domainPattern.test(domainPart)) return false;
-      
-      const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      return re.test(String(value).toLowerCase());
-    }),
+  email: emailValidationSchema,
   country: Yup.string().required('modal.registration.required'),
   firstName: Yup.string()
     .min(2, 'modal.registration.first_name_short')
     .max(50, 'modal.registration.first_name_long')
     .matches(
       /^[a-zA-Zа-щА-ЩґҐєЄіІїЇьЬ\s\-']*[a-zA-Zа-щА-ЩґҐєЄіІїЇьЬ\-']$/,
-      'modal.registration.first_name_invalid_characters',
+      'modal.registration.first_name_invalid_characters'
     )
     .required('modal.registration.required'),
   lastName: Yup.string()
@@ -42,7 +17,7 @@ export const RegistrationSchema = Yup.object().shape({
     .max(50, 'modal.registration.last_name_long')
     .matches(
       /^[a-zA-Zа-щА-ЩґҐєЄіІїЇьЬ\s\-']*[a-zA-Zа-щА-ЩґҐєЄіІїЇьЬ\-']$/,
-      'modal.registration.last_name_invalid_characters',
+      'modal.registration.last_name_invalid_characters'
     )
     .required('modal.registration.required'),
   password: Yup.string()
@@ -50,7 +25,7 @@ export const RegistrationSchema = Yup.object().shape({
     .max(50, 'modal.registration.password_long')
     .matches(
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$&*])[A-Za-z\d!@#$&*]{8,}$/,
-      'modal.registration.password_invalid',
+      'modal.registration.password_invalid'
     )
     .required('modal.registration.required'),
   repeatPassword: Yup.string()
--- src/utils/valadationSchemas/StepContactsSchema.js
+++ src/utils/valadationSchemas/StepContactsSchema.js
@@ -1,4 +1,5 @@
-import * as Yup from 'yup'
+import * as Yup from 'yup';
+import { emailValidationSchema } from './EmailValidationSchema';
 
 const linkedinRegex = /^https:\/\/www\.linkedin\.com\/in\/[a-zA-Z0-9-]+\/?$/;
 const githubRegex = /^https:\/\/github\.com\/[a-zA-Z0-9-]+\/?$/;
@@ -6,21 +7,11 @@ const behanceRegex = /^https:\/\/www\.behance\.net\/[a-zA-Z0-9-]+\/?$/;
 const telegramRegex = /^https:\/\/t\.me\/[a-zA-Z0-9_]+\/?$/;
 
 export const StepContactsSchema = Yup.object().shape({
-  telegram: Yup.string()
-  .matches(telegramRegex, 'please type correct URL, username')
-  .optional(),
-  linkedIn: Yup.string()
-    .matches(linkedinRegex, 'please type correct URL')
-    .optional(),
-  gitHub: Yup.string()
-    .matches(githubRegex, 'please type correct URL')
-    .optional(),
-  behance: Yup.string()
-    .matches(behanceRegex, 'please type correct URL')
-    .optional(),
-  mail: Yup.string()
-    .email('please type correct Email')
-    .required('Email is required'),
+  telegram: Yup.string().matches(telegramRegex, 'please type correct URL, username').optional(),
+  linkedIn: Yup.string().matches(linkedinRegex, 'please type correct URL').optional(),
+  gitHub: Yup.string().matches(githubRegex, 'please type correct URL').optional(),
+  behance: Yup.string().matches(behanceRegex, 'please type correct URL').optional(),
+  mail: emailValidationSchema,
   phone: Yup.string()
     .matches(/^\+?[1-9]\d{1,14}$/, 'please type correct Phone number')
     .optional(),
