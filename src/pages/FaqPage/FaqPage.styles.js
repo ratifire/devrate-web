46e9a35f690f70d213cf150174c913d6caf9e2a1
Vladyslav Dykyi <68863569+VladyslavDykyi@users.noreply.github.com>
2024-11-03 00:45:32 +0200

    FAQ Page v1.0.1

--- src/pages/FaqPage/FaqPage.styles.js
+++ src/pages/FaqPage/FaqPage.styles.js
@@ -19,6 +19,7 @@ export const styles = {
       borderRadius: 2,
       backgroundColor: theme.palette.pagesSections.backgroundColor,
       backgroundImage: 'none',
+      boxShadow: 'none',
     },
   }),
   faq: {
