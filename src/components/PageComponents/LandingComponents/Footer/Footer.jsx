ad1b3e909ea52d058a8df84098dd1b981b17da04
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-11-04 21:11:57 +0100

    added changes

--- src/components/PageComponents/LandingComponents/Footer/Footer.jsx
+++ src/components/PageComponents/LandingComponents/Footer/Footer.jsx
@@ -1,71 +1,135 @@
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import styles from './Footer.styles';
-import { Box, Container, Icon, Link, Typography } from '@mui/material';
-import { Trans, useTranslation } from 'react-i18next';
-import { ReactComponent as LogoBoy } from '../../../../assets/icons/logo-boy.svg';
+import {
+    Box,
+    Container,
+    Link,
+    Typography
+} from '@mui/material';
+import {Trans, useTranslation} from 'react-i18next';
+// import { ReactComponent as LogoBoy } from '../../../../assets/icons/logo-boy.svg';
 import Logo from '../../../UI/Logo';
-import LinkList from '../../../UI/LinkList';
-import legalInfoLinks from '../../../../utils/constants/legalInfoLinks';
+// import LinkList from '../../../UI/LinkList';
+// import legalInfoLinks from '../../../../utils/constants/legalInfoLinks';
 import { Instagram, LinkedIn } from '@mui/icons-material';
+import TextField from "@mui/material/TextField";
 
 const Footer = () => {
   const { t } = useTranslation();
 
   return (
-    <Container maxWidth='xl' sx={styles.container}>
-      <Box component='footer' sx={styles.footer}>
-        <Box sx={styles.wrapper}>
-
-          <Box sx={styles.socialLinks}>
-            <Link to={`/`} component={RouterLink}>
-              <Logo width={'187'} height={'22'} />
-            </Link>
-            <Box sx={styles.socialGroup}>
-              <Link href={'href'} target='_blank'>
-                <LinkedIn sx={styles.icon}/>
-              </Link>
-              <Link href={'href'} target='_blank'>
-                <Instagram sx={styles.icon}/>
-              </Link>
-            </Box>
+      <Container maxWidth='xl' sx={styles.container}>
+        <Box component='footer' sx={styles.footer} >
+          <Box sx={styles.wrapper}>
+              <Box sx={styles.content}>
+                  <Box sx={styles.socialGroup}>
+                      <Link to={`/`} component={RouterLink}>
+                          <Logo width={'187'} height={'22'} />
+                      </Link>
+                      <Box sx={styles.socialLinks}>
+                          <Link href={'href'} target='_blank'>
+                              <Instagram sx={styles.icon}/>
+                          </Link>
+                          <Link href={'href'} target='_blank'>
+                              <LinkedIn sx={styles.icon}/>
+                          </Link>
+                      </Box>
+                  </Box>
+                  <Box component='p' sx={styles.description}>
+                      {t('Technical interview training platform: elevate your skills with both beginners and experienced professionals. Our platform is designed to make the interview preparation process smoother and less stressful for everyone.')}
+                  </Box>
+                  <Box sx={styles.developed}>
+                      <Typography component='h1' sx={styles.developTitle}>
+                          Developed By
+                      </Typography>
+                      <Link to={`/`} component={RouterLink}>
+                          <Logo width={'187'} height={'22'} />
+                      </Link>
+                  </Box>
+                  <Box sx={styles.ratifireLink}>
+                      <Link sx={styles.defaultStylesLinks}>ratifire.org</Link>
+                  </Box>
+                  <Box sx={styles.legalInformation}>
+                      <Link sx={styles.defaultStylesLinks}>Terms and conditions</Link>
+                      <Link sx={styles.defaultStylesLinks}>Privacy Policy</Link>
+                  </Box>
+                  <Box>
+                      <Typography level='body-md' sx={styles.copyright}>
+                          {t('home.footer.copyright')}
+                      </Typography>
+                      <Typography level='body-md' sx={styles.trademarks}>
+                          <Trans i18nKey='multiline'>{t('home.footer.trademarks')}</Trans>
+                      </Typography>
+                  </Box>
+              </Box>
+              <Box sx={styles.contactForm}>
+                  <Typography component='h2' sx={styles.contactFormTitle}>
+                      <span style={styles.spanStyle}>Get</span> In Touch</Typography>
+                  <Box component="form" xs={styles.formInfo}>
+                      <TextField sx={styles.inputStyles}
+                                 id="outlined-user-name"
+                                 name='name'
+                                 label='Name Surname'
+                                 type='text'
+                                 autoComplete='off'
+                                 fullWidth={true}
+                      />
+                      <TextField sx={styles.inputStyles}
+                                 id="outlined-user-email"
+                                 name='email'
+                                 label='Enter your email'
+                                 type='email'
+                                 autoComplete="current-email"
+                                 fullWidth={true}
+                      />
+                      <TextField sx={styles.inputStyles}
+                                 id="outlined-user-request"
+                                 name='request'
+                                 label='Enter your request...'
+                                 type='text'
+                                 rows={4}
+                                 multiline
+                                 autoComplete="off"
+                                 fullWidth={true}
+                      />
+                  </Box>
+              </Box>
           </Box>
-
-
-          {/*<Box sx={styles.wrapperNav}>*/}
-          {/*  <Icon sx={styles.logoBoy}>*/}
-          {/*    <LogoBoy />*/}
-          {/*  </Icon>*/}
-          {/*  <LinkList links={legalInfoLinks} componentStyles={styles} />*/}
-          {/*</Box>*/}
         </Box>
-        <Box>
-          <Box component='paragraph'>
-            {t('Technical interview training platform: elevate your skills with both beginners and experienced professionals. Our platform is designed to make the interview preparation process smoother and less stressful for everyone.')}
-          </Box>
-          <Box sx={{display: 'flex'}}>
-            <Typography component='h2'>
-              {t('Developed By')}
-            </Typography>
-            <Link to={`/`} component={RouterLink}>
-              <Logo width={'187'} height={'22'} />
-            </Link>
-          </Box>
-          <Link>
-            ratifire.org
-          </Link>
-          <Box>
+      </Container>
 
-          </Box>
-          <Typography level='body-md' sx={styles.copyright}>
-            {t('home.footer.copyright')}
-          </Typography>
-          <Typography level='body-md' sx={styles.trademarks}>
-            <Trans i18nKey='multiline'>{t('home.footer.trademarks')}</Trans>
-          </Typography>
-        </Box>
-      </Box>
-    </Container>
   );
 };
 export default Footer;
+// <Container maxWidth='xl' sx={styles.container}>
+//   <Box component='footer' sx={styles.footer}>
+//     <Box sx={styles.wrapper}>
+//       <Link to={`/`} component={RouterLink}>
+//         <Logo width={'187'} height={'22'} />
+//       </Link>
+//       <Box sx={styles.wrapperNav}>
+//         <Icon sx={styles.logoBoy}>
+//           <LogoBoy />
+//         </Icon>
+//         <LinkList links={legalInfoLinks} componentStyles={styles} />
+//         <Box sx={styles.socialGroup}>
+//           <Link href={'href'} target='_blank'>
+//             <Instagram sx={styles.icon}/>
+//           </Link>
+//           <Link href={'href'} target='_blank'>
+//             <LinkedIn sx={styles.icon}/>
+//           </Link>
+//         </Box>
+//       </Box>
+//     </Box>
+//     <Box>
+//       <Typography level='body-md' sx={styles.copyright}>
+//         {t('home.footer.copyright')}
+//       </Typography>
+//       <Typography level='body-md' sx={styles.trademarks}>
+//         <Trans i18nKey='multiline'>{t('home.footer.trademarks')}</Trans>
+//       </Typography>
+//     </Box>
+//   </Box>
+// </Container>
--- src/components/PageComponents/LandingComponents/Footer/Footer.styles.js
+++ src/components/PageComponents/LandingComponents/Footer/Footer.styles.js
@@ -18,29 +18,6 @@ const styles = {
       textAlign: 'left',
     },
   }),
-  copyright: (theme) => ({
-    marginRight: 0,
-    paddingTop: '26px',
-    fontSize: 14,
-    fontWeight: 600,
-    lineHeight: '20px',
-    color: theme.palette.neutral[200],
-    textAlign: 'center',
-    '@media (min-width: 768px)': {
-      textAlign: 'left',
-    },
-  }),
-  trademarks: (theme) => ({
-    fontSize: 14,
-    fontWeight: 600,
-    lineHeight: '20px',
-    paddingTop: '10px',
-    color: theme.palette.neutral[300],
-    textAlign: 'center',
-    '@media (min-width: 768px)': {
-      textAlign: 'left',
-    },
-  }),
   wrapperNav: {
     marginTop: '20px',
     marginLeft: 0,
@@ -61,13 +38,15 @@ const styles = {
   },
   wrapper: {
     display: 'flex',
-    alignItems: 'center',
-    flexDirection: 'column',
-    marginBottom: '20px',
-    '@media (min-width: 768px)': {
-      alignItems: 'flex-end',
-      flexDirection: 'row',
-    },
+    justifyContent: 'space-between',
+    // display: 'flex',
+    // alignItems: 'center',
+    // flexDirection: 'column',
+    // marginBottom: '20px',
+    // '@media (min-width: 768px)': {
+    //   alignItems: 'flex-end',
+    //   flexDirection: 'row',
+    // },
   },
   footer: {
     paddingTop: '170px',
@@ -82,22 +61,115 @@ const styles = {
       marginRight: '36px',
     },
   },
+  content: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  socialGroup: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginBottom: '16px',
+  },
   socialLinks: {
     display: 'flex',
-    gap: '150px',
-
+    gap: '8px',
   },
-  socialGroup: (theme) => ({
-    color: theme.palette.neutral[50],
-    margin: '0 auto',
-    maxWidth: 200,
-    display: 'flex',
-    gridGap: '10px',
-    paddingLeft: '30px',
-    '@media (min-width: 520px)': {},
-  }),
   icon: (theme) => ({
     color: theme.palette.neutral[50],
+    width: '30px',
+    height: '30px',
   }),
+  description: {
+    width: '461px',
+    fontWeight: '600',
+    fontSize: '18px',
+    lineHeight: '150%',
+    margin: '0',
+    marginBottom: '28px',
+  },
+  developed: {
+    display: 'flex',
+    gap: '8px',
+    alignItems: 'center',
+    marginBottom: '21px',
+  },
+  developTitle: {
+    textTransform: 'uppercase',
+    fontSize: '24px',
+    fontWeight: '700',
+  },
+  ratifireLink: {
+    fontWeight: '700',
+    fontSize: '24px',
+    textTransform: 'lowercase',
+    marginBottom: '21px',
+  },
+  legalInformation: {
+    display: 'flex',
+    gap: '24px',
+    fontWeight: '600',
+    fontSize: '20px',
+    marginBottom: '16px',
+  },
+  defaultStylesLinks: {
+    textDecoration: 'underline',
+    textDecorationSkipInk: 'none',
+    color: '#B78AF7',
+  },
+  copyright: (theme) => ({
+    fontSize: '18px',
+    fontWeight: '600',
+    lineHeight: '20px',
+    color: theme.palette.neutral[50],
+    textAlign: 'center',
+    marginBottom:'5px',
+    '@media (min-width: 768px)': {
+      textAlign: 'left',
+    },
+  }),
+  trademarks: (theme) => ({
+    fontSize: '18px',
+    fontWeight: '600',
+    lineHeight: '150%',
+    color: theme.palette.neutral[200],
+    textAlign: 'center',
+    '@media (min-width: 768px)': {
+      textAlign: 'left',
+    },
+  }),
+  contactForm: {
+    width: '650px',
+    backgroundColor: '#514e4e',
+    padding: '36px',
+    borderRadius: '16px',
+  },
+  contactFormTitle: {
+    fontSize: '24px',
+    lineHeight: '150%',
+    marginBottom: '24px',
+  },
+  spanStyle: {
+    color: '#B78AF7',
+  },
+  formInfo: {
+    width: '100%',
+  },
+  inputStyles: {
+    marginBottom: '16px',
+    backgroundColor: '#1D1D1D',
+    borderRadius: '4px',
+  }
+
+
+  // socialGroup: (theme) => ({
+  //   color: theme.palette.neutral[50],
+  //   margin: '0 auto',
+  //   maxWidth: 200,
+  //   display: 'flex',
+  //   gridGap: '10px',
+  //   paddingLeft: '30px',
+  //   '@media (min-width: 520px)': {},
+  // }),
+
 };
 export default styles;
