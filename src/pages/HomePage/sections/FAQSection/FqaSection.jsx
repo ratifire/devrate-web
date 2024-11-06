c2ebd706b0aa4e017c35734093ed259b75b19c07
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-11-06 23:54:56 +0100

    added changes footer sections

--- src/pages/HomePage/sections/FAQSection/FqaSection.jsx
+++ src/pages/HomePage/sections/FAQSection/FqaSection.jsx
@@ -1,20 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Faq.module.scss';
+import { faqData } from './faqData';
+import iconBtn from '../../assets/icon-bottom.svg';
+import iconOpen from '../../assets/icon-open-top.svg';
 
 const FqaSection = () => {
+  const [openId, setOpenId] = useState(null);
+
+  const handleOnClick = (id) => {
+    setOpenId(openId === id ? null : id);
+  };
+
+
+  const leftColumn = faqData.slice(0, 5);
+  const rightColumn = faqData.slice(5);
+
   return (
     <section className={styles.faq}>
       <div className='container'>
         <h1 className={styles.faq__title}>FAQ</h1>
         <div className={styles.faq__menu}>
-          <div className={styles.faq__item}>
-            <button className={styles.faq__header}>What is this platform for?</button>
-            <div className={styles.faq__content}>
-              Answer: Our platform is designed for a comprehensive assessment of technical interviews. Here, you'll
-              engage with more experienced users in your field who will provide objective evaluations and constructive
-              feedback. You will gain valuable practical experience and detailed statistics that will help you better
-              understand your strengths and weaknesses, as well as prepare for real interviews.
-            </div>
+          <div className={styles.faq__items}>
+            {leftColumn.map((item) => (
+              <div className={styles.faq__itemsContent} key={item.id} onClick={() => handleOnClick(item.id)}>
+                <div className={styles.faq__header}>
+                  <img
+                    className={styles.faq__iconBottom}
+                    src={openId === item.id ? iconOpen : iconBtn}
+                    alt='iconButton'
+                  />
+                  <div className={styles.faq__question}>
+                    <span className={styles.faq__number}>{item.id}</span>
+                    {item.question}
+                  </div>
+                  {openId === item.id && <div className={styles.faq__answer}>{item.answer}</div>}
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className={styles.faq__items}>
+            {rightColumn.map((item) => (
+              <div className={styles.faq__itemsContent} key={item.id} onClick={() => handleOnClick(item.id)}>
+                <div className={styles.faq__header}>
+                  <img
+                    className={styles.faq__iconBottom}
+                    src={openId === item.id ? iconOpen : iconBtn}
+                    alt='iconButton'
+                  />
+                  <div className={styles.faq__question}>
+                    <span className={styles.faq__number}>{item.id}</span>
+                    {item.question}
+                  </div>
+                  {openId === item.id && <div className={styles.faq__answer}>{item.answer}</div>}
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
