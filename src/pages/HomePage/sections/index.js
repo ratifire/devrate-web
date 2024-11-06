f918dd7477a2ce30c154f7507df314c0dfcf2cfa
SamZakharov <semen.zaharovv@gmail.com>
2024-11-07 01:12:30 +0200

    AboutUs section need to fix borders

--- src/pages/HomePage/sections/index.js
+++ src/pages/HomePage/sections/index.js
@@ -1,5 +1,6 @@
 import Header from './Header';
 import HeroSection from './HeroSection';
 import Footer from './Footer';
+import AboutUsSection from './AboutUsSection';
 
-export { Header, HeroSection, Footer };
+export { Header, HeroSection, Footer, AboutUsSection };
