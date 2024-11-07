9963f54f89b33ad900358c9320917c33e0b44109
BlackPuma9 <yuliiayavorska9@gmail.com>
2024-11-07 22:49:23 +0100

    New feature: 1.added IconButton to close the window

--- src/layouts/ModalLayout/ModalLayout.styles.js
+++ src/layouts/ModalLayout/ModalLayout.styles.js
@@ -4,22 +4,36 @@ export const styles = {
     justifyContent: 'center',
     alignItems: 'center',
   },
-  modalContainer: {
+  modalContainer: (theme) => ({
+    position: 'relative',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'left',
     padding: '22px 16px',
-    backgroundColor: '#1D1D1D',
-    borderRadius: '6px',
+    backgroundColor: theme.palette.neutral[900],
+    borderRadius: 4,
     width: '95%',
     '@media (min-width: 580px)': {
       width: 570,
       padding: '26px 36px',
     },
-  },
+  }),
   iconContainer: {
     width: 147,
     height: 18,
     marginBottom: '48px',
   },
+  btnIcon: (theme) => ({
+    position: 'absolute',
+    top: theme.spacing(4),
+    right: theme.spacing(4),
+    color: theme.palette.iconBtn.btnClose.color,
+    borderRadius: 1,
+    ':hover': {
+      backgroundColor: theme.palette.iconBtn.btnClose.hoverColor,
+    },
+    svg: {
+      fontSize: '18px',
+    },
+  }),
 };
