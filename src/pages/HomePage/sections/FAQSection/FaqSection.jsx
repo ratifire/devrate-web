e906158c45b578d5b6bcdb084ad32e06f1d6ed8a
Vitaliia Yaremenko <vita.yaremenko97@gmail.com>
2024-11-07 18:31:33 +0100

    added changes FAQ section

--- src/pages/HomePage/sections/FAQSection/FaqSection.jsx
+++ src/pages/HomePage/sections/FAQSection/FaqSection.jsx
@@ -12,17 +12,13 @@ const FaqSection = () => {
   };
 
 
-  const leftColumn = faqData.slice(0, 5);
-  const rightColumn = faqData.slice(5);
-
   return (
     <section className={styles.faq}>
       <div className='container'>
         <h1 className={styles.faq__title}>FAQ</h1>
         <div className={styles.faq__menu}>
-          <div className={styles.faq__items}>
-            {leftColumn.map((item) => (
-              <div className={styles.faq__itemsContent} key={item.id} onClick={() => handleOnClick(item.id)}>
+            {faqData.map((item) => (
+              <div className={styles.faq__item} key={item.id} onClick={() => handleOnClick(item.id)}>
                 <div className={styles.faq__header}>
                   <img
                     className={styles.faq__iconBottom}
@@ -37,25 +33,6 @@ const FaqSection = () => {
                 </div>
               </div>
             ))}
-          </div>
-          <div className={styles.faq__items}>
-            {rightColumn.map((item) => (
-              <div className={styles.faq__itemsContent} key={item.id} onClick={() => handleOnClick(item.id)}>
-                <div className={styles.faq__header}>
-                  <img
-                    className={styles.faq__iconBottom}
-                    src={openId === item.id ? iconOpen : iconBtn}
-                    alt='iconButton'
-                  />
-                  <div className={styles.faq__question}>
-                    <span className={styles.faq__number}>{item.id}</span>
-                    {item.question}
-                  </div>
-                  {openId === item.id && <div className={styles.faq__answer}>{item.answer}</div>}
-                </div>
-              </div>
-            ))}
-          </div>
         </div>
       </div>
     </section>
