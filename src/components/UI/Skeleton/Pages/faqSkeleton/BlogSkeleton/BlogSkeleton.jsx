8b66cb71e33e508ade40047ebbd09fb32d5a615b
Bohdan <boghdan.shimko@gmail.com>
2024-11-08 02:49:51 +0200

    refactor: BlogSkeleton.jsx

--- src/components/UI/Skeleton/Pages/faqSkeleton/BlogSkeleton/BlogSkeleton.jsx
+++ src/components/UI/Skeleton/Pages/faqSkeleton/BlogSkeleton/BlogSkeleton.jsx
@@ -6,10 +6,11 @@ const BlogSkeleton = () => {
   return (
     <Box sx={styles.box}>
       <Skeleton sx={styles.title} variant='rounded' height={32} />
-      <Box>
-        <Skeleton sx={styles.card} variant='rounded' height={171} width={306} />
-        <Skeleton sx={styles.card} variant='rounded' height={171} width={306} />
-        <Skeleton sx={styles.card} variant='rounded' height={171} width={306} />
+      <Box sx={styles.list}>
+        <Skeleton sx={styles.card} variant='rounded' height={280} />
+        <Skeleton sx={styles.card} variant='rounded' height={280} />
+        <Skeleton sx={styles.card} variant='rounded' height={280} />
+        <Skeleton sx={styles.card} variant='rounded' height={280} />
       </Box>
     </Box>
   )
--- src/components/UI/Skeleton/Pages/faqSkeleton/BlogSkeleton/BlogSkeleton.styles.js
+++ src/components/UI/Skeleton/Pages/faqSkeleton/BlogSkeleton/BlogSkeleton.styles.js
@@ -4,8 +4,28 @@ export const styles = {
   }),
   box: (theme) => ({
     padding: theme.spacing(4),
+    paddingBottom: 0,
   }),
   card: {
     marginBottom: '20px',
-  }
+  },
+  list: (theme) => ({
+    maxHeight: '586px',
+    height: '100%',
+    overflowY: 'auto',
+    paddingRight: theme.spacing(3),
+    '&::-webkit-scrollbar': {
+      width: 10,
+    },
+    '&::-webkit-scrollbar-track': {
+      backgroundColor: theme.palette.scroll.scrollWrapp.backgroundColor,
+      borderRadius: 8,
+      marginLeft: '10px',
+      paddingLeft: '10px',
+    },
+    '&::-webkit-scrollbar-thumb': {
+      borderRadius: 6,
+      backgroundColor: theme.palette.scroll.scrollEl.backgroundColor,
+    },
+  })
 };
