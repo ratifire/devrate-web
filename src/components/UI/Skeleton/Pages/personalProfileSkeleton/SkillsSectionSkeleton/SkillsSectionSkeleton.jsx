746b26d7df8a4cd32a1e1421334a567d31713d82
Bohdan <boghdan.shimko@gmail.com>
2024-11-09 22:16:06 +0200

    feat: SkillsSectionSkeleton.jsx

--- src/components/UI/Skeleton/Pages/personalProfileSkeleton/SkillsSectionSkeleton/SkillsSectionSkeleton.jsx
+++ src/components/UI/Skeleton/Pages/personalProfileSkeleton/SkillsSectionSkeleton/SkillsSectionSkeleton.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import {Box, Skeleton} from '@mui/material';
 import { styles } from './SkillsSectionSkeleton.styles';
 
 const SkillsSectionSkeleton = () => {
   return (
-    <Box></Box>
+    <Box sx={styles.wrapper}>
+      <Skeleton sx={styles.elem} variant='rounded' height={32}/>
+      <Skeleton sx={styles.elem} variant='rounded' height={32}/>
+      <Skeleton sx={styles.lastElem} variant='rounded' height={32}/>
+      <Box sx={styles.box}>
+        <Skeleton variant='rounded' width={208} height={72}/>
+        <Skeleton variant='rounded' width={208} height={72}/>
+      </Box>
+    </Box>
   )
 }
 
--- src/components/UI/Skeleton/Pages/personalProfileSkeleton/SkillsSectionSkeleton/SkillsSectionSkeleton.styles.js
+++ src/components/UI/Skeleton/Pages/personalProfileSkeleton/SkillsSectionSkeleton/SkillsSectionSkeleton.styles.js
@@ -1,1 +1,15 @@
-export const styles = {};
+export const styles = {
+  wrapper: (theme) => ({
+    padding: theme.spacing(4),
+  }),
+  elem: (theme) => ({
+    marginBottom: theme.spacing(2),
+  }),
+  lastElem: {
+    marginBottom: '29px',
+  },
+  box: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  }
+};
