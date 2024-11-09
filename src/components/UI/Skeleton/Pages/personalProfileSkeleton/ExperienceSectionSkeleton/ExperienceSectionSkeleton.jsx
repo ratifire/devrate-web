7bfa54be84dea1bd40697dc3d059681c8fb46261
Bohdan <boghdan.shimko@gmail.com>
2024-11-09 22:48:27 +0200

    feat: ExperienceSectionSkeleton.jsx

--- src/components/UI/Skeleton/Pages/personalProfileSkeleton/ExperienceSectionSkeleton/ExperienceSectionSkeleton.jsx
+++ src/components/UI/Skeleton/Pages/personalProfileSkeleton/ExperienceSectionSkeleton/ExperienceSectionSkeleton.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import {Box, Skeleton} from '@mui/material';
 import { styles} from './ExperienceSectionSkeleton.styles';
 
 const ExperienceSectionSkeleton = () => {
   return (
-    <Box></Box>
+    <Box sx={styles.wrapper}>
+      <Box sx={styles.box}>
+        <Skeleton variant='rounded' width={542} height={50}/>
+        <Skeleton variant='rounded' width={50} height={50}/>
+      </Box>
+      <Skeleton variant='rounded' width='100%' height={261}/>
+    </Box>
   )
 }
 
--- src/components/UI/Skeleton/Pages/personalProfileSkeleton/ExperienceSectionSkeleton/ExperienceSectionSkeleton.styles.js
+++ src/components/UI/Skeleton/Pages/personalProfileSkeleton/ExperienceSectionSkeleton/ExperienceSectionSkeleton.styles.js
@@ -1,1 +1,13 @@
-export const styles = {};
+export const styles = {
+  wrapper: (theme) => ({
+    maxWidth: '1110px',
+    width: '100%',
+    padding: theme.spacing(4),
+  }),
+  box: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginBottom: '20px',
+  }
+};
