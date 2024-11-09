4ccdd601d92a0f5c0acada89c612e73640c72e15
Bohdan <boghdan.shimko@gmail.com>
2024-11-10 01:43:24 +0200

    fix: logo src

--- src/utils/constants/pictureData.js
+++ src/utils/constants/pictureData.js
@@ -1,26 +1,37 @@
+import logoLight from '../../assets/icons/LogoHeader/WhiteThemeIcon/logoHeader.svg';
+import logoDark from '../../assets/icons/LogoHeader/BlackThemeIcon/BlackLogoHeader.svg';
+
 export const pictureData = [
   {
     id: '1',
-    src1x: '../../../assets/icons/BlackLogoHeader.svg',
-    src2x: '../../../assets/icons/BlackLogoHeader.svg',
+    src1xl: logoLight,
+    src2xl: logoLight,
+    src1xd: logoDark,
+    src2xd: logoDark,
     mediaWidth: 600,
   },
   {
     id: '2',
-    src1x: '../../../assets/icons/BlackLogoHeader.svg',
-    src2x: '../../../assets/icons/BlackLogoHeader.svg',
+    src1xl: logoLight,
+    src2xl: logoLight,
+    src1xd: logoDark,
+    src2xd: logoDark,
     mediaWidth: 768,
   },
   {
     id: '3',
-    src1x: '../../../assets/icons/BlackLogoHeader.svg',
-    src2x: '../../../assets/icons/BlackLogoHeader.svg',
+    src1xl: logoLight,
+    src2xl: logoLight,
+    src1xd: logoDark,
+    src2xd: logoDark,
     mediaWidth: 1024,
   },
   {
     id: '4',
-    src1x: '../../../assets/icons/BlackLogoHeader.svg',
-    src2x: '../../../assets/icons/BlackLogoHeader.svg',
+    src1xl: logoLight,
+    src2xl: logoLight,
+    src1xd: logoDark,
+    src2xd: logoDark,
     mediaWidth: 1272,
   },
 ];
