98f81866400ca02617df0f51aa151876a8919ef1
Bohdan <boghdan.shimko@gmail.com>
2024-11-09 22:35:33 +0200

    feat: RightSectionSkeleton.jsx

--- src/components/UI/Skeleton/Pages/personalProfileSkeleton/RightSectionSkeleton/RightSectionSkeleton.jsx
+++ src/components/UI/Skeleton/Pages/personalProfileSkeleton/RightSectionSkeleton/RightSectionSkeleton.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import {Box, Skeleton} from '@mui/material';
 import { styles } from './RightSectionSkeleton.styles';
 
 const RightSectionSkeleton = () => {
   return (
-    <Box></Box>
+    <Box sx={styles.wrapper}>
+      <Box sx={styles.titleBox}>
+        <Skeleton variant='rounded' width='100%' height={32}/>
+        <Skeleton variant='rounded' width={40} height={32}/>
+      </Box>
+      <Skeleton sx={styles.contacts} variant='rounded' height={50}/>
+      <Box sx={styles.titleBox}>
+        <Skeleton variant='rounded' width='100%' height={32}/>
+        <Skeleton variant='rounded' width={40} height={32}/>
+      </Box>
+      <Skeleton sx={styles.contacts} variant='rounded' height={120}/>
+      <Skeleton sx={styles.title} variant='rounded' height={32}/>
+      <Skeleton variant='rounded' height={196}/>
+    </Box>
   )
 };
 
--- src/components/UI/Skeleton/Pages/personalProfileSkeleton/RightSectionSkeleton/RightSectionSkeleton.styles.js
+++ src/components/UI/Skeleton/Pages/personalProfileSkeleton/RightSectionSkeleton/RightSectionSkeleton.styles.js
@@ -1,1 +1,18 @@
-export const styles = {};
+export const styles = {
+  wrapper: (theme) => ({
+    padding: theme.spacing(4),
+  }),
+  titleBox: (theme) => ({
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'space-between',
+    gap: '20px',
+    marginBottom: theme.spacing(2),
+  }),
+  contacts: (theme) => ({
+    marginBottom: theme.spacing(4),
+  }),
+  title: (theme) => ({
+    marginBottom: theme.spacing(2),
+  }),
+};
