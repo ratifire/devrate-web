7729a9b7598166ab322ae24ddd2ab7b5ca1736f5
Bohdan <boghdan.shimko@gmail.com>
2024-11-09 19:32:34 +0200

    refactor: HardSkillsChartSkeleton.jsx

--- src/components/UI/Skeleton/Pages/specializationSkeleton/HardSkillsChartSkeleton/HardSkillsChartSkeleton.jsx
+++ src/components/UI/Skeleton/Pages/specializationSkeleton/HardSkillsChartSkeleton/HardSkillsChartSkeleton.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Box, Skeleton } from '@mui/material';
+import { Skeleton } from '@mui/material';
 import { styles } from './HardSkillsChartSkeleton.styles';
 
 const HardSkillsChartSkeleton = () => {
   return (
-    <Box>
+    <>
       <Skeleton sx={styles.title} variant='rounded' height={28} />
       <Skeleton sx={styles.subTitle} variant='rounded' height={50} />
       <Skeleton variant='rounded' height={264} />
-    </Box>
+    </>
   );
 };
 
