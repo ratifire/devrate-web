49f282e19a92f2d77e572ac2c859d081e12a9860
Bohdan <boghdan.shimko@gmail.com>
2024-11-09 22:05:40 +0200

    feat: BaseUserInfoSkeleton.jsx

--- src/components/UI/Skeleton/Pages/personalProfileSkeleton/BaseUserInfoSkeleton/BaseUserInfoSkeleton.jsx
+++ src/components/UI/Skeleton/Pages/personalProfileSkeleton/BaseUserInfoSkeleton/BaseUserInfoSkeleton.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import {Box, Skeleton} from '@mui/material';
 import { styles } from './BaseUserInfoSkeleton.styles';
 
 const BaseUserInfoSkeleton = () => {
   return (
-    <Box></Box>
+    <Box sx={styles.wrapper}>
+      <Box sx={styles.boxAvatar}>
+        <Skeleton variant='rounded' width={152} height={152} />
+        <Box sx={styles.boxInfo}>
+          <Box sx={styles.boxName}>
+            <Skeleton variant='rounded' width='100%' height={30} />
+            <Skeleton variant='rounded' width={34} height={30} />
+          </Box>
+          <Skeleton variant='rounded' width='100%' height={32} />
+          <Skeleton variant='rounded' width='100%' height={28} />
+        </Box>
+      </Box>
+      <Skeleton variant='rounded' width='100' height={44} />
+    </Box>
   )
 };
 
