4805c8e5a07cb03fe5883b5aad178ff950af7c51
Bohdan <boghdan.shimko@gmail.com>
2024-11-09 17:52:58 +0200

    feat: InterviewsSkeleton.jsx

--- src/components/UI/Skeleton/Pages/specializationSkeleton/InterviewsSkeleton/InterviewsSkeleton.jsx
+++ src/components/UI/Skeleton/Pages/specializationSkeleton/InterviewsSkeleton/InterviewsSkeleton.jsx
@@ -1,13 +1,18 @@
-import { Box } from '@mui/material';
+import { Box, Skeleton } from '@mui/material';
 import React from 'react';
 import { styles } from './InterviewsSkeleton.styles';
 
 const InterviewsSkeleton = () => {
   return (
-    <Box>
-
+    <Box sx={styles.wrapper}>
+      <Box sx={styles.box}>
+        <Skeleton variant='rounded' width='100%' height={32} />
+        <Skeleton variant='rounded' width='100%' height={24} />
+        <Skeleton variant='rounded' width='100%' height={24} />
+      </Box>
+      <Skeleton variant='rounded' width='100%' height={44} />
     </Box>
-  )
+  );
 };
 
 export default InterviewsSkeleton;
--- src/components/UI/Skeleton/Pages/specializationSkeleton/InterviewsSkeleton/InterviewsSkeleton.styles.js
+++ src/components/UI/Skeleton/Pages/specializationSkeleton/InterviewsSkeleton/InterviewsSkeleton.styles.js
@@ -1,1 +1,13 @@
-export const styles = {};
+export const styles = {
+  wrapper: (theme) => ({
+    padding: theme.spacing(4),
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '24px',
+  }),
+  box: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '4px',
+  },
+};
