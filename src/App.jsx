ac7184d10b82fabae9668510a5c744f1a570680f
Bohdan <boghdan.shimko@gmail.com>
2024-11-28 13:20:12 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import { RouterProvider } from 'react-router-dom';
 import router from './router/router';
 import 'react-toastify/dist/ReactToastify.css';
 
-console.log('test');
-
 export function App() {
   return <RouterProvider router={router} />;
 }
