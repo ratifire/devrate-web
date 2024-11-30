caf62e94a415024da4d0a767471cfd2a8ff53be4
NewEnergyCode <matveychuk.m@gmail.com>
2024-11-30 11:18:38 +0200

    create multi environments workflow

--- terraform/deploy_service/main.tf
+++ terraform/deploy_service/main.tf
@@ -19,7 +19,7 @@ resource "aws_s3_bucket_policy" "logs_prod_policy" {
         "Service": "logdelivery.elasticloadbalancing.amazonaws.com"
       },
       "Action": "s3:PutObject",
-      "Resource": "arn:aws:s3:::${var.bucket_name_logs}-1209/AWSLogs/${data.aws_caller_identity.current_user.account_id}/*"
+      "Resource": "arn:aws:s3:::${var.bucket_name_logs}/AWSLogs/${data.aws_caller_identity.current_user.account_id}/*"
     }
   ]
 }
