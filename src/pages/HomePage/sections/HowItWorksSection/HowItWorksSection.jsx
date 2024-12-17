0b7a9b5cfa5bc7bd2756b288ef33ae5b23e9e00e
Ruslan <rusik.sorok1337@gmail.com>
2024-12-17 20:36:12 +0200

    fixed bg-s

--- src/pages/HomePage/sections/HowItWorksSection/HowItWorksSection.jsx
+++ src/pages/HomePage/sections/HowItWorksSection/HowItWorksSection.jsx
@@ -16,13 +16,13 @@ const howItWorksSection = () => {
           <Swiper />
           <div className={styles.works__block}>
             <div className={`${styles.works__left} ${styles.left1}`}>
-              <h2 className={styles.mainTitle}>
+              <h3 className={styles.mainTitle}>
                 How It <span>Works?</span>
-              </h2>
+              </h3>
               <div className={`${styles.card__wrapper} ${styles.m1}`}>
                 <div className={styles.card__title__wrapper}>
                   <div className={styles.cricle}>1</div>
-                  <h3 className={styles.card__title}>Register your profile</h3>
+                  <h4 className={styles.card__title}>Register your profile</h4>
                 </div>
                 <p className={styles.card__text}>
                   Create your profile and fill in information about your work experience, hard skills, achievements and
@@ -42,7 +42,7 @@ const howItWorksSection = () => {
               <div className={`${styles.card__wrapper} ${styles.m2}`}>
                 <div className={styles.card__title__wrapper}>
                   <div className={styles.cricle}>2</div>
-                  <h3 className={styles.card__title}>Join the Interview</h3>
+                  <h4 className={styles.card__title}>Join the Interview</h4>
                 </div>
                 <p className={styles.card__text}>
                   Choose whether you want to practice as a respondent or as an interviewer. Pick a convenient time and
@@ -56,7 +56,7 @@ const howItWorksSection = () => {
               <div className={`${styles.card__wrapper} ${styles.m3}`}>
                 <div className={styles.card__title__wrapper}>
                   <div className={styles.cricle}>3</div>
-                  <h3 className={styles.card__title}>Receive Feedback</h3>
+                  <h4 className={styles.card__title}>Receive Feedback</h4>
                 </div>
                 <p className={styles.card__text}>
                   Get detailed feedback to help you improve your soft and hard skills in your area of your
