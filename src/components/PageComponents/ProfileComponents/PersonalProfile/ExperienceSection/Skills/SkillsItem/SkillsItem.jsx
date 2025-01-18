72a25cac85fa17cf12ddd6775c07c29d48af13e9
Ruslan <rusik.sorok1337@gmail.com>
2025-01-18 15:47:52 +0200

    added some fixed

--- src/components/PageComponents/ProfileComponents/PersonalProfile/ExperienceSection/Skills/SkillsItem/SkillsItem.jsx
+++ src/components/PageComponents/ProfileComponents/PersonalProfile/ExperienceSection/Skills/SkillsItem/SkillsItem.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react';
 import { Box, IconButton, Typography } from '@mui/material';
 import PropTypes from 'prop-types';
 import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
 import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined';
-import { useFormik } from 'formik';
+import { useState } from 'react';
 import { useUpdateSkillsMutation } from '../../../../../../../redux/services/skillsApiSlice';
-import { SkillsItemSchema } from '../../../../../../../utils/valadationSchemas/index';
 import CustomTooltip from '../../../../../../UI/CustomTooltip';
 import { styles } from './SkillsItem.styles';
 
@@ -13,38 +11,25 @@ const SkillsItem = ({ data, flex }) => {
   const { id, name, averageMark, hidden } = data;
   const [hiddenSkill, setHiddenSkill] = useState(hidden);
   const mark = Math.round(averageMark);
-
   const [updateSkills] = useUpdateSkillsMutation();
 
-  const onSubmit = () => {
-    updateSkills({
-      id,
-      hide: hiddenSkill,
-    });
+  const handlerClick = async () => {
+    setHiddenSkill((prevState) => !prevState);
+    await updateSkills({ id, hide: !hidden }).unwrap();
   };
-  const formik = useFormik({
-    initialValues: {
-      hidden: hiddenSkill,
-    },
-    validationSchema: SkillsItemSchema,
-    onSubmit,
-  });
+
   const iconEye = hiddenSkill ? (
     <VisibilityOutlinedIcon sx={styles.eye} />
   ) : (
     <VisibilityOffOutlinedIcon sx={styles.eyeHidden} />
   );
-  const handlerClick = () => {
-    setHiddenSkill(!hiddenSkill);
-  };
+
   return (
     <Box sx={[styles.wrapper, flex]}>
       <Box sx={styles.iconWrapper}>
-        <form onSubmit={formik.handleSubmit}>
-          <IconButton sx={styles.icon} type='submit' onClick={handlerClick}>
-            {iconEye}
-          </IconButton>
-        </form>
+        <IconButton sx={styles.icon} type='button' onClick={handlerClick}>
+          {iconEye}
+        </IconButton>
       </Box>
       <Typography sx={styles.text} variant='body1'>
         <CustomTooltip title={name}>{name}</CustomTooltip>
