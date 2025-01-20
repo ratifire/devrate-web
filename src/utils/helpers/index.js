b1529b6316d82dc24ac393f8e4eb66702efdb79a
Ruslan <rusik.sorok1337@gmail.com>
2025-01-20 16:35:45 +0200

    created and filled file dateHandlers, removed unnecessary files

--- src/utils/helpers/index.js
+++ src/utils/helpers/index.js
@@ -1,6 +1,6 @@
 import bgFromString from './bgFromString';
 import checkContrastColor from './checkContrastColor';
-import getUserUTC from './getUserUTC';
+import { getUserUTC } from './dateHandlers.js';
 import getTokenInHeaders from './getTokenInHeaders';
 
 export { bgFromString, checkContrastColor, getUserUTC, getTokenInHeaders };
