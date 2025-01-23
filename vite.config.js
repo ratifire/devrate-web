73c448a9221910ddbf38be90b4b686d3df46cb26
shait <shaitan.vladimir@gmail.com>
2025-01-23 14:52:39 +0200

    init

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
@@ -5,8 +6,13 @@ import svgr from 'vite-plugin-svgr';
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: 3000,
+    host: 'dev.skillzzy.com',
+    port: 443,
     open: true,
+    https: {
+      key: fs.readFileSync('C:\\Windows\\System32\\dev.skillzzy.com+3-key.pem'),
+      cert: fs.readFileSync('C:\\Windows\\System32\\dev.skillzzy.com+3.pem'),
+    },
   },
   build: {
     outDir: 'build',
