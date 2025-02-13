b245bb38e98cac7f0f45cd65d50740c41dcb28a8
Bohdan <boghdan.shimko@gmail.com>
2025-02-13 23:37:39 +0200

    refactor: prepareSkillsDataInterviewSkills.js

--- src/components/PageComponents/SingleScheduledInterview/helpers/prepareSkillsDataInterviewSkills.js
+++ src/components/PageComponents/SingleScheduledInterview/helpers/prepareSkillsDataInterviewSkills.js
@@ -1,3 +1,5 @@
+import matchSkills from './matchSkills.js';
+
 const prepareSkillsDataInterviewSkills = ({ hostSkills, userSkills }) => {
   if (!hostSkills || !userSkills) return;
 
@@ -6,36 +8,10 @@ const prepareSkillsDataInterviewSkills = ({ hostSkills, userSkills }) => {
 
   if (!hardSkillsUser || !softSkillsUser || !hardSkillsHost || !softSkillsHost) return;
 
-  const softSkills = [];
-  const hardSkills = [];
-
-  for (let i = 0; i < hardSkillsHost.length; i++) {
-    for (let j = 0; j < hardSkillsUser.length; j++) {
-      if (hardSkillsHost[i].name === hardSkillsUser[j].name) {
-        hardSkills.push({
-          id: i,
-          name: hardSkillsHost[i].name,
-          leftGrade: hardSkillsHost[i].averageMark,
-          rightGrade: hardSkillsUser[j].averageMark,
-        });
-      }
-    }
-  }
-
-  for (let i = 0; i < softSkillsHost.length; i++) {
-    for (let j = 0; j < softSkillsUser.length; j++) {
-      if (softSkillsHost[i].name === softSkillsUser[j].name) {
-        softSkills.push({
-          id: i,
-          name: softSkillsHost[i].name,
-          leftGrade: softSkillsHost[i].averageMark,
-          rightGrade: softSkillsUser[j].averageMark,
-        });
-      }
-    }
-  }
-
-  return { hardSkills, softSkills };
+  return {
+    hardSkills: matchSkills({ skillsHost: hardSkillsHost, skillsUser: hardSkillsUser }),
+    softSkills: matchSkills({ skillsHost: softSkillsHost, skillsUser: softSkillsUser }),
+  };
 };
 
 export default prepareSkillsDataInterviewSkills;
