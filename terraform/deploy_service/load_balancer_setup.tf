67cdfe843b01afcdb5f56c6f4afb33b324da56c3
NewEnergyCode <matveychuk.m@gmail.com>
2025-02-13 15:16:18 +0200

    stickiness off

--- terraform/deploy_service/load_balancer_setup.tf
+++ terraform/deploy_service/load_balancer_setup.tf
@@ -17,8 +17,7 @@ resource "aws_lb_target_group" "http_ecs_tg_front" {
   vpc_id               = data.aws_vpcs.all_vpcs.ids[0]
   deregistration_delay = "30"
   stickiness {
-    type            = "lb_cookie"
-    cookie_duration = "86400"
+    type = "none"
   }
   health_check {
     healthy_threshold   = 2
