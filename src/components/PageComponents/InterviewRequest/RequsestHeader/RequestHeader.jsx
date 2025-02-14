f14fddb1ff370fc8107aa3bb7629bf9718149bbe
SamZakharov <semen.zaharovv@gmail.com>
2025-02-14 14:56:00 +0200

    I tried to make the layout of RequestHeader as shown in Figma.

--- src/components/PageComponents/InterviewRequest/RequsestHeader/RequestHeader.jsx
+++ src/components/PageComponents/InterviewRequest/RequsestHeader/RequestHeader.jsx
@@ -1,13 +1,92 @@
-import { Box } from '@mui/material';
-import mockRequest from '../mockData.js';
-import styles from './RequestHeader.styles.js';
+import { Box, Typography, IconButton } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import PropTypes from 'prop-types';
+import { useTranslation } from 'react-i18next';
+import AddIcon from '@mui/icons-material/Add';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { ButtonDef } from '../../../FormsComponents/Buttons/index.js';
+import { styles } from './RequestHeader.styles.js';
 
-const RequestHeader = () => {
+const RequestHeader = ({ title, role, description, stats, onAddTimeslot }) => {
+  const { t } = useTranslation();
+
   return (
     <Box sx={styles.container}>
-      <RequestHeader description={mockRequest.description} role={mockRequest.role} title={mockRequest.title} />
+      <Box sx={styles.header}>
+        <Typography sx={styles.title}>
+          {title} as a{' '}
+          <Box
+            component='span'
+            sx={{
+              color: role === 'Respondent' ? styles.respondentColor : styles.interviewerColor,
+            }}
+          >
+            {role}
+          </Box>
+        </Typography>
+
+        <Box sx={styles.buttons}>
+          {role === 'Respondent' && (
+            <IconButton aria-label='delete' sx={styles.iconDelete}>
+              <DeleteIcon />
+            </IconButton>
+          )}
+          <ButtonDef
+            label={t('Аdd time slots')}
+            startIcon={<AddIcon />}
+            sx={styles.outlined}
+            type={'button'}
+            variant='outlined'
+            onClick={onAddTimeslot}
+          />
+
+          <Box sx={styles.menuIcon}>
+            {/*<IconButton sx={styles.openMenuDots} onClick={(event) => handleMenuOpen(event)}>*/}
+            <IconButton sx={styles.openMenuDots}>
+              <MoreVertIcon />
+            </IconButton>
+          </Box>
+        </Box>
+      </Box>
+
+      <Box sx={styles.statsContainer}>
+        <Typography component={'strong'} sx={styles.statItem} variant={'subtitle3'}>
+          Знайдено інтерв’ю:{' '}
+          <Typography component='strong' sx={styles.foundInterviews} variant={'subtitle3'}>
+            {stats.foundInterviews}
+          </Typography>
+        </Typography>
+        <Typography component={'strong'} sx={styles.statItem} variant={'subtitle3'}>
+          Кількість інтерв’ю:{' '}
+          <Typography component='strong' sx={styles.totalInterviews} variant={'subtitle3'}>
+            {stats.totalInterviews}
+          </Typography>
+        </Typography>
+        <Typography component={'strong'} sx={styles.statItem} variant={'subtitle3'}>
+          Обрано таймслотів:{' '}
+          <Typography component='strong' sx={styles.selectedTimeslots} variant={'subtitle3'}>
+            {stats.selectedTimeslots}
+          </Typography>
+        </Typography>
+      </Box>
+
+      <Box sx={styles.description}>
+        <Typography variant={'body'}>{description}</Typography>
+      </Box>
     </Box>
   );
+};
+
+RequestHeader.propTypes = {
+  title: PropTypes.string.isRequired,
+  role: PropTypes.oneOf(['Respondent', 'Interviewer']).isRequired,
+  description: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    foundInterviews: PropTypes.number.isRequired,
+    totalInterviews: PropTypes.number.isRequired,
+    selectedTimeslots: PropTypes.number.isRequired,
+  }).isRequired,
+  onAddTimeslot: PropTypes.func.isRequired,
 };
 
 export default RequestHeader;
---
+++ src/components/PageComponents/InterviewRequest/RequsestHeader/RequestHeader.styles.js
@@ -0,0 +1,91 @@
+export const styles = {
+  container: {
+    marginBottom: '24px',
+  },
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: '20px',
+    fontWeight: 'bold',
+    marginBottom: '8px',
+  },
+  respondentColor: (theme) => theme.palette.requestInterviewe.role.respondent,
+  interviewerColor: (theme) => theme.palette.requestInterviewe.role.interviewer,
+
+  foundInterviews: (theme) => ({
+    color: theme.palette.requestInterviewe.interviewCount.foundInterviews,
+  }),
+  totalInterviews: (theme) => ({
+    color: theme.palette.requestInterviewe.interviewCount.totalInterviews,
+  }),
+  selectedTimeslots: (theme) => ({
+    color: theme.palette.requestInterviewe.interviewCount.selectedTimeslots,
+  }),
+
+  menuIcon: (theme) => ({
+    color: theme.palette.text.secondary,
+  }),
+  openMenuDots: (theme) => ({
+    flex: '1 0 35px',
+    color: theme.palette.requestInterviewe.buttons.openMenuDots.color,
+    borderRadius: 1,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    transition: 'background-color 0.5s, transform 0.5s',
+    '&:hover': {
+      color: theme.palette.requestInterviewe.buttons.openMenuDots.hover.color,
+      backgroundColor: theme.palette.requestInterviewe.buttons.openMenuDots.hover.backgroundColor,
+    },
+  }),
+
+  statsContainer: {
+    display: 'flex',
+    gap: '16px',
+    marginBottom: '16px',
+  },
+  statItem: (theme) => ({
+    color: theme.palette.requestInterviewe.statItem.color,
+  }),
+  description: (theme) => ({
+    color: theme.palette.requestInterviewe.description.color,
+    marginBottom: '16px',
+  }),
+  buttons: {
+    display: 'flex',
+    alignItems: 'center',
+    gap: '8px',
+  },
+  iconDelete: (theme) => ({
+    flex: '1 0 35px',
+    borderRadius: 1,
+    color: theme.palette.requestInterviewe.buttons.delete.color,
+    '&:hover': {
+      color: theme.palette.requestInterviewe.buttons.delete.hover.color,
+      backgroundColor: theme.palette.requestInterviewe.buttons.delete.hover.backgroundColor,
+    },
+  }),
+  outlined: (theme) => ({
+    width: '216px',
+    marginLeft: '16px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    paddingY: '13px',
+    paddingX: '22px',
+    color: theme.palette.iconBtn.bookInterview.color,
+    fontFamily: theme.typography.fontFamily,
+    border: `1px solid ${theme.palette.iconBtn.bookInterview.borderColor}`,
+    textTransform: 'lowercase',
+    '&::first-letter': {
+      textTransform: 'uppercase',
+    },
+    '&:hover': {
+      backgroundColor: theme.palette.iconBtn.bookInterview.hover.backgroundColor,
+      borderColor: theme.palette.iconBtn.bookInterview.hover.borderColor,
+      color: theme.palette.iconBtn.bookInterview.hover.color,
+    },
+  }),
+};
