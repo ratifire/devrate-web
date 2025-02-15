ab6a0f29e9bf8a959245351ae13d319021a36ca9
Bohdan <boghdan.shimko@gmail.com>
2025-02-15 22:21:00 +0200

    feat: ScheduledInterviewsGuard.jsx

---
+++ src/router/guards/ScheduledInterviewsGuard.jsx
@@ -0,0 +1,23 @@
+import { Outlet, useNavigate } from 'react-router';
+import { useEffect } from 'react';
+import { useGetAllScheduledInterviewsQuery } from '../../redux/interviews/scheduledInterviewsApiSlice';
+import navigationLinks from '../links';
+
+const ScheduledInterviewsGuard = () => {
+  const { data: scheduledInterviews } = useGetAllScheduledInterviewsQuery({ page: 1, size: 5 });
+  const navigate = useNavigate();
+  const firstInterviewId = scheduledInterviews?.content[0].id;
+  const event = scheduledInterviews?.content[0];
+
+  useEffect(() => {
+    if (firstInterviewId) {
+      navigate(`${navigationLinks.scheduledInterviews}/${firstInterviewId}`, {
+        state: { event },
+      });
+    }
+  }, [firstInterviewId]);
+
+  return <Outlet />;
+};
+
+export default ScheduledInterviewsGuard;
