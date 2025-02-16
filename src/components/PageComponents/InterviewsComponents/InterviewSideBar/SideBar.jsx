01f6f0e819319f386ae340417d26ab452b350b80
Bohdan <boghdan.shimko@gmail.com>
2025-02-17 01:56:55 +0200

    feat: useGetHeightElement.js

--- src/components/PageComponents/InterviewsComponents/InterviewSideBar/SideBar.jsx
+++ src/components/PageComponents/InterviewsComponents/InterviewSideBar/SideBar.jsx
@@ -1,33 +1,27 @@
 import { Box, Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
-import { useLayoutEffect, useRef } from 'react';
+import { useRef } from 'react';
 import { useTheme } from '@mui/material/styles';
 import SideBarEvent from '../InterviewSideBarEvent';
 import { useScrollPadding } from '../../../../utils/helpers/useScrollPadding';
+import useGetHeightElement from '../../../../utils/hooks/useGetHeightElement.js';
 import { styles } from './SideBar.styles';
 
 const SideBar = ({ interviews, refHandler, passedInterview }) => {
   const { t } = useTranslation();
   const theme = useTheme();
   const containerRef = useRef(null);
-  const heightRef = useRef(null);
+  const ref = useGetHeightElement('.ScheduledInterviewsPage');
 
   useScrollPadding(containerRef, '9px');
-
-  useLayoutEffect(() => {
-    if (!heightRef.current) {
-      const parentNode = document.querySelector('.ScheduledInterviewsPage');
-      heightRef.current = parentNode?.clientHeight || 1068;
-    }
-  }, []);
 
   return (
     <Box sx={styles.wrapper}>
       <Typography sx={styles.interviewTitle} variant='h4'>
         {t('interviews.sideBar.title')}
       </Typography>
-      <Box ref={containerRef} sx={styles.scrollContainer(theme, heightRef.current)}>
+      <Box ref={ref} sx={styles.scrollContainer(theme, ref.current)}>
         {interviews?.map((event, index) => (
           <SideBarEvent
             key={event.id}
