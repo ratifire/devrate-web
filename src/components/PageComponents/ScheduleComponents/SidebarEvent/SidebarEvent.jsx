6f50fac8ee9904eec3167af197ed48925fb4abb7
Bohdan <boghdan.shimko@gmail.com>
2025-03-21 00:03:19 +0200

    #1272 | refactor: SidebarEvent.jsx

--- src/components/PageComponents/ScheduleComponents/SidebarEvent/SidebarEvent.jsx
+++ src/components/PageComponents/ScheduleComponents/SidebarEvent/SidebarEvent.jsx
@@ -5,18 +5,14 @@ import { useMemo } from 'react';
 import { Box, Button, IconButton, Paper, Typography, Link } from '@mui/material';
 import LinkIcon from '@mui/icons-material/Link';
 import { useTranslation } from 'react-i18next';
-import { useSelector } from 'react-redux';
-import { useDeleteEvent } from '@utils/hooks/useDeleteEvent';
+import useDeleteEvent from '@utils/hooks/useDeleteEvent';
 import useCheckTimeDifference from '@utils/hooks/schedule/useCheckTimeDifference.js';
+import { lvlMastery } from '@utils/constants/masteryLvl.js';
 import { styles } from './SidebarEvent.styles';
 
 const SidebarEvent = ({ event }) => {
-  const { eventTypeId, type, link, host, startTime, participantDtos } = event;
-  const { id: hostId, name, surname } = host;
-
-  const { id: userId } = useSelector((state) => state.auth.user.data);
+  const { hostName, hostSurname, roomUrl, id, startTime, type, masteryLevel, specializationName } = event;
   const { t } = useTranslation();
-
   const optionsDate = { day: '2-digit', month: '2-digit', year: 'numeric', separator: '/', localeMatcher: 'lookup' };
   const optionsTime = { hour: 'numeric', minute: 'numeric', hour12: false };
 
@@ -32,20 +28,15 @@ const SidebarEvent = ({ event }) => {
 
   const handleCancelInterview = async () => {
     await deleteEvent({
-      userId,
-      eventId: event?.eventTypeId,
-      // eslint-disable-next-line no-console
-      onError: (error) => console.error('EventPopup: Error deleting event', error),
-      // eslint-disable-next-line no-console
-      onFinally: () => console.log('EventPopup: Deletion process complete'),
+      eventId: id,
     });
   };
 
   return (
-    <Paper key={eventTypeId} sx={styles.sideBarEventContainer}>
+    <Paper sx={styles.sideBarEventContainer}>
       <Box sx={styles.titleDateTimeBox}>
         <Typography component='div' sx={styles.title} variant='h6'>
-          {type.toLowerCase()}
+          {type}
         </Typography>
         <Typography component='div' sx={styles.dateAndTime} variant='body2'>
           {customFormattedDate} {formattedTime}
@@ -53,25 +44,19 @@ const SidebarEvent = ({ event }) => {
       </Box>
       <Typography component='div' sx={styles.host} variant='body2'>
         {t('schedule.host')}:{' '}
-        <Link component={RouterLink} sx={styles.host_link} to={`/profile/${hostId}`}>
-          {name} {surname}
-        </Link>
-      </Typography>
-      <Typography component='div' sx={styles.participant} variant='body2'>
-        {t('schedule.role')}:{' '}
-        <Link component={RouterLink} sx={styles.participant_link} to={`/profile/${participantDtos[0].id}`}>
-          {participantDtos[0].name} {participantDtos[0].surname}
+        <Link component={RouterLink} sx={styles.host_link} to={`/profile/${id}`}>
+          {hostName} {hostSurname}
         </Link>
       </Typography>
       <Typography component='div' sx={styles.hostTitle} variant='caption3'>
-        {participantDtos[0].status}
+        {lvlMastery[masteryLevel]} {specializationName}
       </Typography>
       <Box sx={styles.titleDateTimeBox}>
-        <IconButton component='a' disabled={disableLink} href={link} target='_blank'>
+        <IconButton component='a' disabled={disableLink} href={roomUrl} target='_blank'>
           <LinkIcon />
         </IconButton>
         {showCancelButton && (
-          <Button sx={styles.cancelEventBtn} variant='text' onClick={() => handleCancelInterview(eventTypeId)}>
+          <Button sx={styles.cancelEventBtn} variant='text' onClick={() => handleCancelInterview(id)}>
             {t('schedule.cancelEventBtn')}
           </Button>
         )}
@@ -81,26 +66,15 @@ const SidebarEvent = ({ event }) => {
 };
 
 SidebarEvent.propTypes = {
-  setEventUpdated: PropTypes.func,
   event: PropTypes.shape({
-    eventTypeId: PropTypes.number.isRequired,
-    type: PropTypes.string.isRequired,
-    link: PropTypes.string.isRequired,
-    host: PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      name: PropTypes.string.isRequired,
-      surname: PropTypes.string.isRequired,
-      status: PropTypes.string.isRequired,
-    }).isRequired,
+    hostName: PropTypes.string.isRequired,
+    hostSurname: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    masteryLevel: PropTypes.number.isRequired,
+    roomUrl: PropTypes.string.isRequired,
+    specializationName: PropTypes.string.isRequired,
     startTime: PropTypes.string.isRequired,
-    participantDtos: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        name: PropTypes.string.isRequired,
-        surname: PropTypes.string.isRequired,
-        status: PropTypes.string.isRequired,
-      })
-    ),
+    type: PropTypes.string.isRequired,
   }).isRequired,
 };
 
