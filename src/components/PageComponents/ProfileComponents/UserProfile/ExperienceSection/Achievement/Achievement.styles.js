dfc0800d0ca732347dd5562d06cbf8ee22048590
vladyslavdykyi <vladislavdykyi33@gmail.com>
2025-04-08 05:11:27 +0300

    #1319

---
+++ src/components/PageComponents/ProfileComponents/UserProfile/ExperienceSection/Achievement/Achievement.styles.js
@@ -0,0 +1,8 @@
+export const styles = {
+  achievementItemContainer: (theme) => ({
+    width: '100%',
+    display: 'flex',
+    flexWrap: 'wrap',
+    gridGap: theme.spacing(3),
+  }),
+};
--- src/components/PageComponents/ProfileComponents/UserProfile/ExperienceSection/Achievement/AchievementItem/AchievementItem.jsx
+++ src/components/PageComponents/ProfileComponents/UserProfile/ExperienceSection/Achievement/AchievementItem/AchievementItem.jsx
@@ -1,38 +1,36 @@
-import { Box, Link, Typography, Tooltip, Grid } from '@mui/material';
-import LinkIcon from '@mui/icons-material/Link';
+import { Box, Typography } from '@mui/material';
+// import LinkIcon from '@mui/icons-material/Link';
 import PropTypes from 'prop-types';
 import styles from './AchievementItem.styles';
 
 const AchievementItem = ({ achievement, icon: IconComponent }) => {
   return (
-    <Grid item xs={6}>
-      <Box sx={styles.achievementItemContainer}>
-        <Box sx={styles.itemHeaderContainer}>
-          {IconComponent && <IconComponent height={48} width={48} />}
-          <Box sx={styles.logoTitleContainer}>
-            <Box sx={styles.achievementTitleYearContainer}>
-              <Typography sx={styles.achievementTitle} variant='h6'>
-                {achievement.summary}
-              </Typography>
-            </Box>
-            {/*{achievement.link && (*/}
-            <Box sx={{ position: 'relative' }}>
-              <Tooltip arrow title={achievement.link}>
-                {/*commented out <Link> in case if its need it's needed in the future*/}
-                <Link href={achievement.link} sx={styles.link} target='_blank' underline='none'>
-                  {/*<Link  sx={styles.link} underline="none">*/}
-                  <LinkIcon />
-                </Link>
-              </Tooltip>
-            </Box>
-            {/*// )}*/}
+    <Box sx={styles.achievementItemContainer}>
+      <Box sx={styles.itemHeaderContainer}>
+        {IconComponent && <IconComponent height={48} width={48} />}
+        <Box sx={styles.logoTitleContainer}>
+          <Box sx={styles.achievementTitleYearContainer}>
+            <Typography sx={styles.achievementTitle} variant='h6'>
+              {achievement.summary}
+            </Typography>
           </Box>
+          {/*{achievement.link && (*/}
+          {/*<Box sx={{ position: 'relative' }}>*/}
+          {/*  <Tooltip arrow title={achievement.link}>*/}
+          {/*    commented out <Link> in case if its need it's needed in the future*/}
+          {/*    <Link href={achievement.link} sx={styles.link} target='_blank' underline='none'>*/}
+          {/*      <Link  sx={styles.link} underline="none">*/}
+          {/*      <LinkIcon />*/}
+          {/*    </Link>*/}
+          {/*  </Tooltip>*/}
+          {/*</Box>*/}
+          {/*// )}*/}
         </Box>
-        <Typography sx={styles.achievementItemText} variant='body1'>
-          {achievement.description}
-        </Typography>
       </Box>
-    </Grid>
+      <Typography sx={styles.achievementItemText} variant='body1'>
+        {achievement.description}
+      </Typography>
+    </Box>
   );
 };
 
