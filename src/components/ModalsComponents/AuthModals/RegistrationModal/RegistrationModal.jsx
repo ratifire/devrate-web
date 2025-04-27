3586a9ef64f8f023c2217ad4e7cb9daa3a5b677e
mykola.tsyb <mykola.tsyb@oracle.com>
2025-04-27 21:53:09 +0200

    Revert "#1350 | delete: unused fields"
    This reverts commit 7408bccdd6f2f60d31031af5053ac036f079e41d.

--- src/components/ModalsComponents/AuthModals/RegistrationModal/RegistrationModal.jsx
+++ src/components/ModalsComponents/AuthModals/RegistrationModal/RegistrationModal.jsx
@@ -36,12 +36,14 @@ const RegistrationModal = () => {
   };
 
   const onSubmit = async (values, { resetForm, setErrors }) => {
-    const { email, firstName, lastName, password } = values;
+    const { email, firstName, lastName, country, news, password } = values;
     try {
       await createUser({
         email,
         firstName,
         lastName,
+        country: country.target.value,
+        subscribed: news,
         password,
       }).unwrap();
 
