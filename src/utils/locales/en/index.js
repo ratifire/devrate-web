093187764686f17170702dd1e0d59ac064faed7e
mykola.tsyb <mykola.tsyb@oracle.com>
2025-04-27 21:53:18 +0200

    Revert "#1350 | refactor: delete unused UI elements in RegistrationModal.jsx and refactor locales"
    This reverts commit e948f85b9c2c45785ebb4425338a78abfd4f8dbc.

--- src/utils/locales/en/index.js
+++ src/utils/locales/en/index.js
@@ -84,6 +84,8 @@ export const en = {
         community: 'Community',
         contacts: 'Contacts',
         login: 'log in',
+        terms_and_conditions: 'Terms and Conditions',
+        privacy_policy: 'Privacy Policy',
       },
     },
     modal: {
@@ -176,6 +178,8 @@ export const en = {
         password_tooltip:
           'Password must contain 6 characters, 1 uppercase letter, 1 digit, 1 special character no spaces',
         password_invalid: 'Incorrect password',
+        news_letter: 'Send me news, surveys and special offers from SKILLZZY',
+        agreement: 'I have read and agree to the Terms and conditions',
         agreement_error: 'To use our service, you must agree to the terms of use',
         btn_register: 'Sign Up',
         privacy_policy: 'Privacy Policy',
