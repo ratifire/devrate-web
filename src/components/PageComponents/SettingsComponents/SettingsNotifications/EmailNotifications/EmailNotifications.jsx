b69418d1713512c92b7b8fcf3a11c9a30be49e5c
Bohdan <boghdan.shimko@gmail.com>
2025-05-12 01:34:11 +0300

    #1420 | refactor: authSlice.js

--- src/components/PageComponents/SettingsComponents/SettingsNotifications/EmailNotifications/EmailNotifications.jsx
+++ src/components/PageComponents/SettingsComponents/SettingsNotifications/EmailNotifications/EmailNotifications.jsx
@@ -1,9 +1,18 @@
 import { useTranslation } from 'react-i18next';
 import { Box, Switch, Typography } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleSubscribedEmailNotification } from '@redux/slices/auth/authSlice.js';
 import { styles } from './EmailNotifications.styles';
 
 const EmailNotifications = () => {
   const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const { data } = useSelector((state) => state.auth.user);
+  const subscribed = data?.subscribed;
+
+  const handleChangeSubscription = () => {
+    dispatch(toggleSubscribedEmailNotification());
+  };
 
   return (
     <>
@@ -11,7 +20,7 @@ const EmailNotifications = () => {
         <Typography component='h5' variant='h5'>
           {t('settings.notifications.email.title')}
         </Typography>
-        <Switch sx={styles.switch} />
+        <Switch checked={subscribed} sx={styles.switch} onChange={handleChangeSubscription} />
       </Box>
       <Typography component='p' variant='body'>
         {t('settings.notifications.email.text')}
