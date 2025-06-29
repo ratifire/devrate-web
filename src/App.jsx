141409fafc9edb9df9675fac8c3ffc5a5afddf42
Bohdan <boghdan.shimko@gmail.com>
2025-06-29 14:51:43 +0300

    save docker

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,5 @@ import { RouterProvider } from 'react-router/dom';
 import router from './router/router.jsx';
 
 export function App() {
-  // console.log('App component rendered');
   return <RouterProvider router={router} />;
 }
