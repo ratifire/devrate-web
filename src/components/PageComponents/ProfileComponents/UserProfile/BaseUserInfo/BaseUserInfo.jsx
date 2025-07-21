75633a993fb14f7f11f82d231f94e131ce33a597
RuslanSoroka <114746058+RuslanSoroka@users.noreply.github.com>
2025-07-21 15:12:51 +0300

    Revert "#1494 #1498 "

--- src/components/PageComponents/ProfileComponents/UserProfile/BaseUserInfo/BaseUserInfo.jsx
+++ src/components/PageComponents/ProfileComponents/UserProfile/BaseUserInfo/BaseUserInfo.jsx
@@ -10,7 +10,7 @@ import { useGetAvatarUserQuery } from '@redux/api/slices/user/avatar/avatarApiSl
 import UserAvatar from '@components/UI/UserAvatar';
 // import Bookmark from '@components/UI/Bookmark';
 import { ButtonDef } from '@components/FormsComponents/Buttons';
-import { useMemo } from 'react';
+import { makeCorrectCountryCase } from '@utils/helpers/caseHandler.js';
 import { styles } from './BaseUserInfo.styles';
 
 const BaseUserInfo = ({ id }) => {
@@ -19,17 +19,13 @@ const BaseUserInfo = ({ id }) => {
   const { t } = useTranslation();
   // const [isBookmarked, setIsBookmarked] = useState(false);
   const userData = personalData || {};
-  const { firstName: userFirstName, lastName: userLastName, country, city, status } = userData;
-
-  const displayData = useMemo(
-    () => ({
-      firstName: userFirstName ?? '',
-      lastName: userLastName ?? '',
-      country: country ?? '',
-      city: city ?? '',
-    }),
-    [userFirstName, userLastName, country, city]
-  );
+  const {
+    firstName: getFirstName,
+    lastName: getLastName,
+    country: getCountry,
+    city: getCity,
+    status: getStatus,
+  } = userData;
 
   const { data } = useGetAvatarUserQuery(id);
   const userAvatar = data || {};
@@ -39,9 +35,7 @@ const BaseUserInfo = ({ id }) => {
   // };
 
   const handleWriteMessage = () => {
-    dispatch(
-      openChat({ id, firstName: displayData.firstName, lastName: displayData.lastName, userPicture: userPicture })
-    );
+    dispatch(openChat({ id, firstName: getFirstName, lastName: getLastName, userPicture: userPicture }));
   };
 
   const handleBookInterview = () => {
@@ -52,34 +46,31 @@ const BaseUserInfo = ({ id }) => {
   return (
     <Box sx={styles.wrapper}>
       <Box sx={styles.wrapperAvatar}>
-        {userFirstName && userLastName && (
+        {getFirstName && getLastName && (
           <UserAvatar
             correctStyle={styles.correctAvatar}
             radius='square'
             size='l'
             src={userPicture}
-            userFirstName={displayData.firstName}
-            userLastName={displayData.lastName}
-            userName={`${displayData.firstName} ${displayData.lastName}`}
+            userFirstName={getFirstName}
+            userLastName={getLastName}
+            userName={`${getFirstName} ${getLastName}`}
           />
         )}
       </Box>
       <Box sx={styles.wrapperText}>
         <Typography sx={styles.userName} variant='h5'>
-          {`${displayData.firstName} ${displayData.lastName}`}
+          {`${getFirstName} ${getLastName}`}
           {/*<Bookmark isBookmarked={isBookmarked} onToggle={handleToggleBookmark} />*/}
         </Typography>
         <Typography sx={styles.speciality} variant='subtitle1'>
-          {status || ''}
+          {getStatus || ''}
         </Typography>
-        {(displayData.city || displayData.country) && (
-          <Typography sx={styles.city} variant='subtitle2'>
-            <LocationOnIcon sx={styles.icon} />
-            {displayData.city && `${displayData.city},`} {t(`countriesList.${displayData.country}`)}
-          </Typography>
-        )}
+        <Typography sx={styles.city} variant='subtitle2'>
+          <LocationOnIcon sx={styles.icon} />
+          {`${getCity ? getCity + ',' : ''} ${t(`countriesList.${makeCorrectCountryCase(getCountry)}`)}`}
+        </Typography>
       </Box>
-
       <Box sx={styles.buttons}>
         <ButtonDef
           label={t('profile.baseUserInfo.writeMessage')}
