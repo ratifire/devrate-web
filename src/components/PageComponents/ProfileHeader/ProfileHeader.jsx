ef0e18ff0272372a2bb723926853293899f59c28
SamZakharov <semen.zaharovv@gmail.com>
2025-07-23 12:07:08 +0300

    Changed user profile photo to DehazeIcon by MUI. Deleted previous code from component and changed styles according to Figma.

--- src/components/PageComponents/ProfileHeader/ProfileHeader.jsx
+++ src/components/PageComponents/ProfileHeader/ProfileHeader.jsx
@@ -1,14 +1,10 @@
 import { AppBar, Box, Button } from '@mui/material';
 import { useState } from 'react';
-import { useSelector } from 'react-redux';
 import { Link } from 'react-router';
-import { selectCurrentUser } from '@redux/slices/auth/authSlice';
-import { useGetAvatarUserQuery } from '@redux/api/slices/user/avatar/avatarApiSlice';
-import { useGetPersonalUserQuery } from '@redux/api/slices/user/personal/personalApiSlice';
 import links from '@router/links';
 import Logo from '@components/UI/Logo';
 import ThemeSwitch from '@components/UI/ThemeSwitch/ThemeSwitch';
-import UserAvatar from '@components/UI/UserAvatar';
+import DehazeIcon from '@mui/icons-material/Dehaze';
 import Menu from '../Menu';
 import Notification from '../Notification';
 import Chat from '../Chat';
@@ -16,15 +12,7 @@ import { InputSearch } from './InputSearch';
 import styles from './ProfileHeader.styles';
 
 const ProfileHeader = () => {
-  const { data: info } = useSelector(selectCurrentUser);
-  const { id, firstName, lastName } = info;
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-  const { data: personalData } = useGetPersonalUserQuery(id);
-  const { firstName: getFirstName, lastName: getLastName } = personalData || {};
-
-  const { data } = useGetAvatarUserQuery(id);
-  const userAvatar = data || {};
-  const { userPicture } = userAvatar;
 
   const toggleDrawer = (event) => {
     if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
@@ -50,13 +38,7 @@ const ProfileHeader = () => {
         <Notification />
         <Chat />
         <Button sx={styles.userPhoto} onClick={toggleDrawer}>
-          <UserAvatar
-            radius='square'
-            size='sm'
-            src={userPicture}
-            userFirstName={getFirstName || firstName}
-            userLastName={getLastName || lastName}
-          />
+          <DehazeIcon sx={styles.dehazeIcon} />
         </Button>
         <Menu closeMenu={handleCloseMenu} isDrawerOpen={isDrawerOpen} toggleDrawer={toggleDrawer} />
       </Box>
