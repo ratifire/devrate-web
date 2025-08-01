43034843494f065224a9ba5e2bb239d8ae18af83
Ruslan <rusik.sorok1337@gmail.com>
2025-08-01 20:15:01 +0300

    #1485 added state of return url in case when token is expired

--- src/redux/api/apiSlice.js
+++ src/redux/api/apiSlice.js
@@ -20,10 +20,11 @@ const baseQuery = fetchBaseQuery({
 });
 
 const baseQueryWithReauth = async (args, api, extraOptions) => {
+  const currentUrl = window.location.href;
   const result = await baseQuery(args, api, extraOptions);
 
   if (result.error && result.error.status === 401) {
-    api.dispatch(logOut());
+    api.dispatch(logOut(currentUrl));
     api.dispatch(clearTokens());
     return result;
   }
@@ -41,7 +42,7 @@ const baseQueryWithReauth = async (args, api, extraOptions) => {
       );
 
       if (refreshToken.error && refreshToken.error.status === 497) {
-        api.dispatch(logOut());
+        api.dispatch(logOut(currentUrl));
         api.dispatch(clearTokens());
         return result;
       }
@@ -60,7 +61,7 @@ const baseQueryWithReauth = async (args, api, extraOptions) => {
       }
       // eslint-disable-next-line no-unused-vars
     } catch (error) {
-      api.dispatch(logOut());
+      api.dispatch(logOut(currentUrl));
       api.dispatch(clearTokens());
       return result;
     }
