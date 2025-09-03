b12321df31b336797ddb3e0718f4c3c82c28f60d
Bohdan <boghdan.shimko@gmail.com>
2025-09-04 00:06:28 +0300

    #1580 | refactor: PreviewVideoPassedInterview.jsx

---
+++ src/components/PageComponents/SinglePassedInterviewComponents/PreviewVideoPassedInterview/PreviewVideoPassedInterview.jsx
@@ -0,0 +1,143 @@
+import { Box, Typography } from '@mui/material';
+import ReactPlayer from 'react-player';
+import { useState } from 'react';
+import { feedbackInterviewRole } from '@utils/constants/feedbackInterviewRole';
+import { useGetPersonalUserQuery } from '@redux/api/slices/user/personal/personalApiSlice';
+import { useParams } from 'react-router';
+import { useGetPassedInterviewByIdQuery } from '@redux/api/slices/interviews/passedInterviewsApiSlice';
+import InterviewPreviewVideo from '@components/PageComponents/InterviewsComponents/InterviewPreviewVideo';
+import { useGetAvatarUserQuery } from '@redux/api/slices/user/avatar/avatarApiSlice';
+import { lvlMastery } from '@utils/constants/masteryLvl';
+import { ErrorComponent } from '@components/UI/Exceptions';
+import { useTranslation } from 'react-i18next';
+import { styles } from './PreviewVideoPassedInterview.styles';
+
+const PreviewVideoPassedInterview = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const { interviewId } = useParams();
+  const { t } = useTranslation();
+  const {
+    data: interviewData,
+    isFetching: isFetchingPassedInterview,
+    isError: isErrorPassedInterview,
+  } = useGetPassedInterviewByIdQuery({ interviewId }, { skip: !interviewId });
+  const attendeeId = interviewData?.attendeeId;
+  const {
+    data: avatar,
+    isFetching: isFetchingAvatar,
+    isError: isErrorAvatar,
+  } = useGetAvatarUserQuery(attendeeId, { skip: !attendeeId });
+  const { attendeeMasteryLevel, attendeeSpecialization, videoUrl = interviewData?.videoUrl } = interviewData;
+  const interviewerId = interviewData?.userId;
+  const {
+    data: candidateContacts,
+    isFetching: isFetchingCandidateContacts,
+    isError: isErrorCandidateContacts,
+  } = useGetPersonalUserQuery(interviewerId, {
+    skip: !interviewerId,
+  });
+  const {
+    data: userContacts,
+    isFetching: isFetchingUserContacts,
+    isError: isErrorUserContacts,
+  } = useGetPersonalUserQuery(attendeeId, {
+    skip: !attendeeId,
+  });
+  const {
+    data: interviewerAvatar,
+    isFetching: isFetchingInterviewerAvatar,
+    isError: isErrorInterviewerAvatar,
+  } = useGetAvatarUserQuery(interviewerId, {
+    skip: !interviewerId,
+  });
+
+  const showPreview = !isPlaying || !!videoUrl;
+
+  const isError =
+    isErrorPassedInterview ||
+    isErrorUserContacts ||
+    isErrorCandidateContacts ||
+    isErrorAvatar ||
+    isErrorInterviewerAvatar;
+  const isFetching =
+    isFetchingPassedInterview ||
+    isFetchingUserContacts ||
+    isFetchingCandidateContacts ||
+    isFetchingInterviewerAvatar ||
+    isFetchingAvatar;
+
+  if (isFetching) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <ErrorComponent />;
+  }
+
+  const role = interviewData?.role;
+  const level = lvlMastery[attendeeMasteryLevel];
+  const { firstName, lastName } = userContacts;
+  const { firstName: candidateFirstName, lastName: candidateLastName } = candidateContacts;
+
+  const isCandidateRole = role === feedbackInterviewRole.CANDIDATE;
+
+  const previewData = {
+    candidate: {
+      firstName: isCandidateRole ? candidateFirstName : firstName,
+      lastName: isCandidateRole ? candidateLastName : lastName,
+      avatar: isCandidateRole ? avatar?.userPicture : interviewerAvatar?.userPicture,
+    },
+    interviewer: {
+      firstName: isCandidateRole ? firstName : candidateFirstName,
+      lastName: isCandidateRole ? lastName : candidateLastName,
+      avatar: isCandidateRole ? interviewerAvatar?.userPicture : avatar?.userPicture,
+    },
+    level: level || '',
+    specialization: attendeeSpecialization || '',
+    role: role || '',
+  };
+
+  const handleVideoEnded = () => {
+    setIsPlaying(false);
+  };
+
+  const handlePlayPressed = () => {
+    setIsPlaying(true);
+  };
+
+  return (
+    <Box sx={styles.container}>
+      <Box sx={styles.header}>
+        <Typography sx={styles.title}>{t('interviews.passedInterviews.interviewPreviewVideo.headerTitle')}</Typography>
+      </Box>
+      {showPreview ? (
+        <InterviewPreviewVideo
+          shouldShowVisibilityControl
+          candidateFirstName={previewData?.candidate?.firstName}
+          candidateLastName={previewData?.candidate?.lastName}
+          candidateSrc={previewData?.interviewer?.avatar}
+          interviewLevel={previewData?.level}
+          interviewerFirstName={previewData?.interviewer?.firstName}
+          interviewerLastName={previewData?.interviewer?.lastName}
+          interviewerSrc={previewData?.candidate?.avatar}
+          specialization={previewData?.specialization}
+          onPlayPressed={handlePlayPressed}
+        />
+      ) : (
+        <Box sx={styles.playerWrapper}>
+          <ReactPlayer
+            controls
+            height='100%'
+            playing={isPlaying}
+            src={videoUrl}
+            style={styles.interviewVideo}
+            width='100%'
+            onEnded={handleVideoEnded}
+          />
+        </Box>
+      )}
+    </Box>
+  );
+};
+
+export default PreviewVideoPassedInterview;
--- src/pages/InterviewPages/SinglePassedInterviewPage/SinglePassedInterviewPage.jsx
+++ src/pages/InterviewPages/SinglePassedInterviewPage/SinglePassedInterviewPage.jsx
@@ -1,4 +1,3 @@
-import { ErrorComponent } from '@components/UI/Exceptions/index.js';
 import {
   InterviewFeedbackSkeleton,
   InterviewInfoSkeleton,
@@ -8,102 +7,61 @@ import {
 } from '@components/UI/Skeleton';
 import { Box, Paper, Typography } from '@mui/material';
 import { useGetPassedInterviewByIdQuery } from '@redux/api/slices/interviews/passedInterviewsApiSlice.js';
-import { useGetAvatarUserQuery } from '@redux/api/slices/user/avatar/avatarApiSlice.js';
-import { useGetPersonalUserQuery } from '@redux/api/slices/user/personal/personalApiSlice.js';
-import { lvlMastery } from '@utils/constants/masteryLvl.js';
 import { DARK_THEME } from '@utils/constants/Theme/theme.js';
-import { formatToLocalDateInterview } from '@utils/helpers/formatToLocalDateInterview.js';
-import { lazy, memo, Suspense, useCallback, useEffect, useMemo, useState } from 'react';
+import { lazy, memo, Suspense } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router';
-import ReactPlayer from 'react-player';
 import { feedbackInterviewRole } from '@utils/constants/feedbackInterviewRole.js';
-import EmptySkills from '../../../components/UI/Specialization/EmptySkills';
-
 import EmptyRequestPicDark from '../../../assets/pictures/emptyInterviewTabsPictures/requestInterview/requestDark.svg?react';
 import EmptyRequestPicLight from '../../../assets/pictures/emptyInterviewTabsPictures/requestInterview/requestLight.svg?react';
 import { styles } from './SingleIPassednterviewPage.styles.js';
 
-const InterviewInfo = lazy(() => import('../../../components/PageComponents/InterviewsComponents/InterviewInfo'));
-const InterviewHardSkills = lazy(
-  () => import('../../../components/PageComponents/InterviewsComponents/InterviewHardSkills')
+const UserCardSinglePassedInterview = lazy(
+  () => import('@components/PageComponents/SinglePassedInterviewComponents/UserCardSinglePassedInterview')
 );
-const InterviewSoftSkills = lazy(
-  () => import('../../../components/PageComponents/InterviewsComponents/InterviewSoftSkills')
-);
-const Statistics = lazy(() => import('../../../components/PageComponents/InterviewsComponents/InterviewStatistics'));
-const InterviewFeedback = lazy(
-  () => import('../../../components/PageComponents/InterviewsComponents/InterviewFeedback')
-);
-const UserCard = lazy(async () => {
-  const module = await import('../../../components/UI/Interview');
-  return { default: module.UserCard };
-});
 
-const InterviewPreviewVideo = lazy(
-  () => import('../../../components/PageComponents/InterviewsComponents/InterviewPreviewVideo')
+const PreviewVideoPassedInterview = lazy(
+  () => import('@components/PageComponents/SinglePassedInterviewComponents/PreviewVideoPassedInterview')
+);
+
+const StatisticPassedInterview = lazy(
+  () => import('@components/PageComponents/SinglePassedInterviewComponents/StatisticPassedInterview')
+);
+
+const InterviewSoftSkillsSinglePassedInterview = lazy(
+  () => import('@components/PageComponents/SinglePassedInterviewComponents/InterviewSoftSkillsSinglePassedInterview')
+);
+
+const InterviewHardSkillsSinglePassedInterview = lazy(
+  () => import('@components/PageComponents/SinglePassedInterviewComponents/InterviewHardSkillsSinglePassedInterview')
+);
+
+const InterviewInfo = lazy(() => import('../../../components/PageComponents/InterviewsComponents/InterviewInfo'));
+
+const InterviewFeedback = lazy(
+  () => import('@components/PageComponents/SinglePassedInterviewComponents/PassedInterviewFeedback')
 );
 
 const MemoizedInterviewInfo = memo(InterviewInfo);
-const MemoizedInterviewHardSkills = memo(InterviewHardSkills);
-const MemoizedInterviewSoftSkills = memo(InterviewSoftSkills);
-const MemoizedStatistics = memo(Statistics);
+const MemoizedInterviewHardSkillsSinglePassedInterview = memo(InterviewHardSkillsSinglePassedInterview);
+const MemoizedInterviewSoftSkillsSinglePassedInterview = memo(InterviewSoftSkillsSinglePassedInterview);
 const MemoizedInterviewFeedback = memo(InterviewFeedback);
-const MemoizedUserCard = memo(UserCard);
-const MemoizedInterviewPreviewVideo = memo(InterviewPreviewVideo);
+const MemoizedUserCardSinglePassedInterview = memo(UserCardSinglePassedInterview);
+const MemoizedPreviewVideoPassedInterview = memo(PreviewVideoPassedInterview);
 
 const SinglePassedInterviewPage = () => {
   const { t } = useTranslation();
-  const { interviewId } = useParams();
   const { mode } = useSelector((state) => state.theme);
+  const { interviewId } = useParams();
   const { data: interviewData, isFetching: isFetchingPassedInterview } = useGetPassedInterviewByIdQuery(
     { interviewId },
     { skip: !interviewId }
   );
-  const [isPlaying, setIsPlaying] = useState(false);
 
-  useEffect(() => {
-    setIsPlaying(false);
-  }, [interviewId]);
-
-  const handlePlayPressed = useCallback(() => {
-    setIsPlaying(true);
-  }, []);
-  const attendeeId = interviewData?.attendeeId ?? '';
-  const interviewerId = interviewData?.userId ?? '';
   const role = interviewData?.role;
 
-  const { data: userContacts, isFetching: isFetchingContacts } = useGetPersonalUserQuery(attendeeId, {
-    skip: !attendeeId,
-  });
-
-  const { data: candidateContacts } = useGetPersonalUserQuery(interviewerId, {
-    skip: !interviewerId,
-  });
-
-  const {
-    data: avatar,
-    isFetching: isFetchingAvatar,
-    isError: isErrorAvatar,
-  } = useGetAvatarUserQuery(attendeeId, { skip: !attendeeId });
-
-  const { data: interviewerAvatar, isError: isErrorInterviewerAvatar } = useGetAvatarUserQuery(interviewerId, {
-    skip: !interviewerId,
-  });
-
-  const {
-    dateTime = new Date(),
-    hardSkills = {},
-    softSkills = {},
-    feedback = '',
-    attendeeMasteryLevel = '',
-    attendeeSpecialization = '',
-    videoUrl = interviewData?.videoUrl,
-  } = interviewData ?? {};
-
-  const hasVideoUrl = Boolean(videoUrl);
-  const showPreview = !isPlaying || !hasVideoUrl;
+  const { hardSkills = {}, softSkills = {} } = interviewData ?? {};
 
   const getSkillsArray = (skillsArray) =>
     Object.entries(skillsArray).map(([name, averageMark]) => ({
@@ -122,78 +80,18 @@ const SinglePassedInterviewPage = () => {
   const averageHardSkillsMark = getAverageSkillsMark(hardSkillsArray);
   const averageSoftSkillsMark = getAverageSkillsMark(softSkillsArray);
 
-  const { firstName = '', lastName = '' } = userContacts ?? {};
-  const { firstName: candidateFirstName = '', lastName: candidateLastName = '' } = candidateContacts ?? {};
-  const userRole = lvlMastery[attendeeMasteryLevel] + ' ' + attendeeSpecialization;
-  const level = lvlMastery[attendeeMasteryLevel];
-
   const EmptyInterviewSvg = mode === DARK_THEME ? EmptyRequestPicDark : EmptyRequestPicLight;
 
   const hasStatistics =
     (role === feedbackInterviewRole.CANDIDATE && averageHardSkillsMark > 0) || averageSoftSkillsMark > 0;
 
-  const isFetchingUserCard = isFetchingContacts || isFetchingAvatar || isFetchingPassedInterview;
-
-  const previewData = useMemo(() => {
-    const isCandidateRole = role === feedbackInterviewRole.CANDIDATE;
-
-    return {
-      candidate: {
-        firstName: isCandidateRole ? candidateFirstName : firstName,
-        lastName: isCandidateRole ? candidateLastName : lastName,
-        avatar: isCandidateRole ? avatar?.userPicture : interviewerAvatar?.userPicture,
-      },
-      interviewer: {
-        firstName: isCandidateRole ? firstName : candidateFirstName,
-        lastName: isCandidateRole ? lastName : candidateLastName,
-        avatar: isCandidateRole ? interviewerAvatar?.userPicture : avatar?.userPicture,
-      },
-      level: level || '',
-      specialization: attendeeSpecialization || '',
-      role: role || '',
-    };
-  }, [
-    role,
-    candidateFirstName,
-    candidateLastName,
-    firstName,
-    lastName,
-    avatar,
-    interviewerAvatar,
-    level,
-    attendeeSpecialization,
-  ]);
-
-  const handleVideoEnded = useCallback(() => {
-    setIsPlaying(false);
-  }, []);
-
-  if (isErrorAvatar || isErrorInterviewerAvatar) {
-    return <ErrorComponent />;
-  }
-
   return (
     <Box className='InterviewsPage' sx={styles.mainContent}>
       <Paper sx={styles.userInfo}>
-        {isFetchingUserCard ? (
-          <UserCardSkeleton />
-        ) : (
-          <Suspense fallback={<UserCardSkeleton />}>
-            <MemoizedUserCard
-              date={formatToLocalDateInterview(dateTime)}
-              firstName={firstName}
-              id={attendeeId}
-              isViewBtn={false}
-              label='label'
-              lastName={lastName}
-              lvl={level}
-              role={userRole}
-              src={avatar?.userPicture}
-            />
-          </Suspense>
-        )}
+        <Suspense fallback={<UserCardSkeleton />}>
+          <MemoizedUserCardSinglePassedInterview />
+        </Suspense>
       </Paper>
-
       <Paper sx={styles.interviewInfo}>
         <Suspense fallback={<InterviewInfoSkeleton />}>
           <MemoizedInterviewInfo />
@@ -206,95 +104,34 @@ const SinglePassedInterviewPage = () => {
               {t('interviews.passedInterviews.interviewersAssessmentTitle')}
             </Typography>
             <Box sx={styles.skillsWrapper}>
-              {role === feedbackInterviewRole.CANDIDATE && (
-                <Paper sx={styles.hardSkills}>
+              <Paper sx={styles.hardSkills}>
+                {role === feedbackInterviewRole.CANDIDATE && (
                   <Suspense fallback={<SkillsSkeleton />}>
-                    {hardSkillsArray.length > 0 || isFetchingPassedInterview ? (
-                      <MemoizedInterviewHardSkills
-                        averageHardSkillsMark={averageHardSkillsMark}
-                        hardSkills={hardSkillsArray}
-                      />
-                    ) : (
-                      <EmptySkills title='Hard skills' />
-                    )}
+                    <MemoizedInterviewHardSkillsSinglePassedInterview />
                   </Suspense>
-                </Paper>
-              )}
-
+                )}
+              </Paper>
               <Paper sx={styles.sortSkills}>
                 <Suspense fallback={<SkillsSkeleton />}>
-                  {softSkillsArray.length > 0 || isFetchingPassedInterview ? (
-                    <MemoizedInterviewSoftSkills
-                      averageSoftSkillsMark={averageSoftSkillsMark}
-                      softSkills={softSkillsArray}
-                    />
-                  ) : (
-                    <EmptySkills title='Soft skills' />
-                  )}
+                  <MemoizedInterviewSoftSkillsSinglePassedInterview />
                 </Suspense>
               </Paper>
             </Box>
           </Paper>
-
           <Paper sx={styles.statistics}>
-            {isFetchingPassedInterview ? (
-              <StatisticSkeleton />
-            ) : (
-              <Suspense fallback={<StatisticSkeleton />}>
-                <MemoizedStatistics
-                  hardSkillMark={role === feedbackInterviewRole.CANDIDATE ? averageHardSkillsMark : 0}
-                  softSkillMark={averageSoftSkillsMark}
-                />
-              </Suspense>
-            )}
+            <Suspense fallback={<StatisticSkeleton />}>
+              <StatisticPassedInterview />
+            </Suspense>
           </Paper>
-
-          {(feedback || isFetchingPassedInterview) && (
-            <Paper sx={styles.interviewFeedback}>
-              {isFetchingPassedInterview ? (
-                <InterviewFeedbackSkeleton />
-              ) : (
-                <Suspense fallback={<InterviewFeedbackSkeleton />}>
-                  <MemoizedInterviewFeedback feedbackText={feedback} />
-                </Suspense>
-              )}
-            </Paper>
-          )}
-
+          <Paper sx={styles.interviewFeedback}>
+            <Suspense fallback={<InterviewFeedbackSkeleton />}>
+              <MemoizedInterviewFeedback />
+            </Suspense>
+          </Paper>
           <Paper sx={styles.interviewPreviewVideo}>
-            <Box sx={styles.container}>
-              <Box sx={styles.header}>
-                <Typography sx={styles.title}>
-                  {t('interviews.passedInterviews.interviewPreviewVideo.headerTitle')}
-                </Typography>
-              </Box>
-              {showPreview ? (
-                <MemoizedInterviewPreviewVideo
-                  shouldShowVisibilityControl
-                  candidateFirstName={previewData.candidate.firstName}
-                  candidateLastName={previewData.candidate.lastName}
-                  candidateSrc={previewData.interviewer.avatar}
-                  interviewLevel={previewData.level}
-                  interviewerFirstName={previewData.interviewer.firstName}
-                  interviewerLastName={previewData.interviewer.lastName}
-                  interviewerSrc={previewData.candidate.avatar}
-                  specialization={previewData.specialization}
-                  onPlayPressed={handlePlayPressed}
-                />
-              ) : (
-                <Box sx={styles.playerWrapper}>
-                  <ReactPlayer
-                    controls
-                    height='100%'
-                    playing={isPlaying}
-                    src={videoUrl}
-                    style={styles.interviewVideo}
-                    width='100%'
-                    onEnded={handleVideoEnded}
-                  />
-                </Box>
-              )}
-            </Box>
+            <Suspense fallback={'loading'}>
+              <MemoizedPreviewVideoPassedInterview />
+            </Suspense>
           </Paper>
         </>
       ) : (
