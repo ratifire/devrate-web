c4aa85cd6f697ee1cb4f8853e0cc032eddc3e296
Bohdan <boghdan.shimko@gmail.com>
2025-09-04 00:22:05 +0300

    #1580 | refactor: InterviewInfoSkeleton.jsx

--- src/components/UI/Skeleton/Pages/interviewSkeleton/InterviewInfoSkeleton/InterviewInfoSkeleton.jsx
+++ src/components/UI/Skeleton/Pages/interviewSkeleton/InterviewInfoSkeleton/InterviewInfoSkeleton.jsx
@@ -6,21 +6,21 @@ const InterviewInfoSkeleton = () => {
     <Box sx={styles.interviewInfoWrapper}>
       <Box sx={styles.interviewInfoTitleWrapper}>
         <Box width='50%'>
-          <Skeleton height={32} variant='text' width='100%' />
+          <Skeleton height={32} variant='rounded' width='100%' />
           <Skeleton height={24} width='45%' />
         </Box>
-        <Skeleton height={24} variant='text' width='35%' />
+        <Skeleton height={24} variant='rounded' width='35%' />
       </Box>
-      <Skeleton height={1} sx={styles.line} variant='text' width='100%' />
+      <Skeleton height={1} sx={styles.line} variant='rounded' width='100%' />
       <Box sx={styles.interviewSpecializationTitleWrapper}>
-        <Skeleton height={32} variant='text' width='40%' />
-        <Skeleton height={28} variant='text' width='18%' />
+        <Skeleton height={32} variant='rounded' width='40%' />
+        <Skeleton height={28} variant='rounded' width='18%' />
       </Box>
-      <Skeleton height={24} sx={styles.interviewRole} variant='text' width='40%' />
+      <Skeleton height={24} sx={styles.interviewRole} variant='rounded' width='40%' />
       <Box sx={styles.hostWrapper}>
-        <Skeleton height={24} variant='text' width='40%' />
+        <Skeleton height={24} variant='rounded' width='40%' />
       </Box>
-      <Skeleton height={17} variant='text' width='40%' />
+      <Skeleton height={17} variant='rounded' width='40%' />
     </Box>
   );
 };
