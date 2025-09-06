7ab6c53e85c030f386eb128687240c6a71493083
Bohdan <boghdan.shimko@gmail.com>
2025-09-06 18:44:00 +0300

    #1580 | refactor: EmptyStatisticsSkeleton.jsx

--- src/components/UI/Skeleton/Pages/interviewSkeleton/EmptyStatisticsSkeleton/EmptyStatisticsSkeleton.jsx
+++ src/components/UI/Skeleton/Pages/interviewSkeleton/EmptyStatisticsSkeleton/EmptyStatisticsSkeleton.jsx
@@ -1,7 +1,7 @@
 import { Skeleton } from '@mui/material';
 
 const EmptyStatisticsSkeleton = () => {
-  return <Skeleton height={'100%'} variant={'rounded'} width={'100%'} />;
+  return <Skeleton height={'420px'} variant={'rounded'} width={'100%'} />;
 };
 
 export default EmptyStatisticsSkeleton;
